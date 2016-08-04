666115d53a67838dbacca7d0b257c2f0b055716b
Peter Peterson <petey@yahoo-inc.com>
2016-08-04 10:29:12 -0700

    fix tests for components

--- tests/integration/components/home-hero/component-test.js
+++ tests/integration/components/home-hero/component-test.js
@@ -11,14 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{home-hero}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#home-hero}}
-      template block text
-    {{/home-hero}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('h1').text().trim(), 'screw·driv·er');
 });
--- tests/integration/components/sd-header/component-test.js
+++ tests/integration/components/sd-header/component-test.js
@@ -11,14 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{sd-header}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#sd-header}}
-      template block text
-    {{/sd-header}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$('img').attr('src').match(/assets\/screwdriver\.png$/));
 });
