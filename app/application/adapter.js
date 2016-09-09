5294c5876163fd320e9f3972d6dd190fb69d6667
Peter Peterson <petey@yahoo-inc.com>
2016-09-09 13:04:01 -0700

    add withCredentials to api requests

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -3,5 +3,24 @@ import ENV from 'screwdriver-ui/config/environment';
 
 export default DS.RESTAdapter.extend({
   namespace: ENV.APP.SDAPI_NAMESPACE,
-  host: ENV.APP.SDAPI_HOSTNAME
+  host: ENV.APP.SDAPI_HOSTNAME,
+
+  /**
+   * Add cors support to all ajax calls
+   * @method ajax
+   * @param  {String} url    the url for the calls
+   * @param  {String} method the type of call eg. GET POST
+   * @param  {Object} hash   configuration object for the call
+   * @return {Promise}
+   */
+  ajax(url, method, hash) {
+    const finalHash = hash || {};
+
+    finalHash.crossDomain = true;
+    finalHash.xhrFields = {
+      withCredentials: true
+    };
+
+    return this._super(url, method, finalHash);
+  }
 });
