c180f7805808c8df7920324f73516362392ecdbd
Peter Peterson <petey@yahoo-inc.com>
2016-09-15 18:57:40 -0700

    make a global application adapter

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -32,5 +32,82 @@ export default DS.RESTAdapter.extend({
    */
   headers: Ember.computed(function cHeaders() {
     return { Authorization: `Bearer ${this.get('session').get('data.authenticated.token')}` };
-  }).volatile()
+  }).volatile(),
+
+  /**
+   * Interface for adding content to a payload before handleResponse is complete
+   * Ideally, this could be handled by a model specific adapter or serializer, but Ember doesn't use
+   * the correct [foo] adapter when making calls to /pipeline/:id/foo
+   */
+  decoratePayload(key, payload) {
+    if (Array.isArray(payload[key])) {
+      payload[key].map(o => this.insertLink(key, o));
+    } else {
+      this.insertLink(key, payload[key]);
+    }
+  },
+  insertLink(key, o) {
+    if (!o) {
+      return;
+    }
+
+    if (key === 'pipeline' || key === 'pipelines') {
+      o.links = {
+        jobs: 'jobs',
+        secrets: 'secrets'
+      };
+    } else if (key === 'job' || key === 'jobs') {
+      o.links = {
+        builds: 'builds'
+      };
+    }
+  },
+
+  /**
+   * Overriding default adapter because our API doesn't provide model names around request data
+   * https://github.com/emberjs/data/blob/v2.7.0/addon/adapters/rest.js#L883
+   * @method handleResponse
+   * @param  {Number}       status      response status
+   * @param  {Object}       headers     response headers
+   * @param  {Object}       payload     response payload
+   * @param  {Object}       requestData original request info
+   * @return {Object | DS.AdapterError} response
+   */
+  handleResponse(status, headers, payload, requestData) {
+    // handle generically when there is an error key in the payload
+    if (payload.error) {
+      return this._super(status, headers, { errors: payload }, requestData);
+    }
+
+    let data = {};
+    let key;
+
+    // urls are of the form: https://server.com/namespace/key1s/:id/key2s, but :id and key2s are optional
+    const urlParser = new RegExp(
+      `${ENV.APP.SDAPI_HOSTNAME}/${ENV.APP.SDAPI_NAMESPACE}/([^/]+)(/([^/]+))?(/([^/]+))?`
+    );
+    const matches = requestData.url.match(urlParser);
+
+    // catch if we got a really weird url
+    if (!matches) {
+      // bail
+      return this._super(...arguments);
+    }
+
+    // the last key on the path and remove the s at the end
+    key = matches[5] || matches[1];
+    key = key.substr(0, key.length - 1);
+
+    // Fix our API not returning the model name in payload
+    if (payload && Array.isArray(payload)) {
+      key = `${key}s`;
+      data[key] = payload;
+    } else if (payload) {
+      data[key] = payload;
+    }
+    this.decoratePayload(key, data);
+
+    // Pass-through to super-class
+    return this._super(status, headers, data, requestData);
+  }
 });
--- tests/unit/application/adapter-test.js
+++ tests/unit/application/adapter-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 import Pretender from 'pretender';
-
+import DS from 'ember-data';
 let server;
 
 moduleFor('adapter:application', 'Unit | Adapter | application', {
@@ -37,4 +37,86 @@ test('it uses cors for ajax', function (assert) {
   return adapter.ajax('fakeurl', 'GET').then(response => {
     assert.deepEqual(response, { foo: 'bar' });
   });
+});
+
+test('it wraps non-array payload with model name', function (assert) {
+  let adapter = this.subject();
+
+  const requestData = {
+    url: 'http://localhost:8080/v3/builds/12345'
+  };
+
+  const payload = adapter.handleResponse(200, {}, { id: 1234 }, requestData);
+
+  assert.deepEqual(payload, {
+    build: { id: 1234 }
+  });
+});
+
+test('it wraps array payload with model name', function (assert) {
+  let adapter = this.subject();
+
+  const requestData = {
+    url: 'http://localhost:8080/v3/builds'
+  };
+
+  const payload = adapter.handleResponse(200, {}, [{ id: 1234 }], requestData);
+
+  assert.deepEqual(payload, {
+    builds: [{ id: 1234 }]
+  });
+});
+
+test('it adds links to pipelines', function (assert) {
+  let adapter = this.subject();
+
+  const requestData = {
+    url: 'http://localhost:8080/v3/pipelines/1234324'
+  };
+
+  const payload = adapter.handleResponse(200, {}, { id: 1234 }, requestData);
+
+  assert.deepEqual(payload, {
+    pipeline: { id: 1234, links: { jobs: 'jobs', secrets: 'secrets' } }
+  });
+});
+
+test('it adds links to jobs', function (assert) {
+  let adapter = this.subject();
+
+  const requestData = {
+    url: 'http://localhost:8080/v3/pipelines/1234/jobs'
+  };
+
+  const payload = adapter.handleResponse(200, {}, [{ id: 1234 }], requestData);
+
+  assert.deepEqual(payload, {
+    jobs: [{ id: 1234, links: { builds: 'builds' } }]
+  });
+});
+
+test('it adds links to jobs', function (assert) {
+  let adapter = this.subject();
+
+  const requestData = {
+    url: 'http://localhost:8080/v3/pipelines/1234/jobs'
+  };
+
+  const payload = adapter.handleResponse(200, {}, [], requestData);
+
+  assert.deepEqual(payload, {
+    jobs: []
+  });
+});
+
+test('it wraps errors', function (assert) {
+  let adapter = this.subject();
+
+  const requestData = {
+    url: 'http://localhost:8080/v3/pipelines/1234/jobs'
+  };
+
+  const payload = adapter.handleResponse(404, {}, { error: 'bananas' }, requestData);
+
+  assert.ok(payload instanceof DS.AdapterError);
 });
