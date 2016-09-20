05f1d442957e744841408d7afb40021af44d8ea0
Peter Peterson <petey@yahoo-inc.com>
2016-09-20 14:29:40 -0700

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: 'module'
   },
-  extends: 'airbnb-base',
+  extends: 'screwdriver',
   env: {
     'browser': true
   },
@@ -12,27 +12,9 @@ module.exports = {
     "ansi_up": true
   },
   rules: {
-    'comma-dangle': ['error', 'never'],
     indent: ['error', 2],
-    'max-len': ['error', { code: 100, ignoreComments: true }],
-    'new-cap': ['error'],
-    'newline-after-var': ['error', 'always'],
-    'newline-before-return': 'error',
-    'no-bitwise': 'error',
-    'no-multiple-empty-lines': ['error', { max: 1 }],
-    'no-param-reassign': ['error', { props: false }],
     'no-underscore-dangle': 'off',
     'prefer-const': 'off',
-    'prefer-rest-params': 'off',
-    'prefer-spread': 'off',
-    'require-jsdoc': ['error', {
-        require: {
-            FunctionDeclaration: true,
-            MethodDefinition: false,
-            ClassDeclaration: false
-        }
-    }],
-    strict: ['error', 'safe'],
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/no-mutable-exports': 'off',
