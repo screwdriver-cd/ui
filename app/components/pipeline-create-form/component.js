8f8a1ceb5738171e13f8a669068cde2acf9ba3c6
Peter Peterson <jedipetey@gmail.com>
2016-12-02 11:45:23 -0800

    refactor: simplify the create page ux by removing extra steps

--- app/components/pipeline-create-form/component.js
+++ app/components/pipeline-create-form/component.js
@@ -2,87 +2,45 @@
 import Ember from 'ember';
 import git from '../../utils/git';
 
-let LOCALSTORAGE_KEY = 'create-pipeline-data';
-
 export default Ember.Component.extend({
-  currentStep: 1,
+  scmUrl: '',
+  isSaving: false,
+  isValid: Ember.computed('scmUrl', {
+    get() {
+      const val = this.get('scmUrl');
 
-  /**
-   * Fires before the template is originally drawn
-   * @event willInsertElement
-   */
-  init() {
-    let scmUrl = this.get('scmUrl');
-
-    if (git.parse(scmUrl).valid) {
-      this.set('currentStep', 2);
-    }
-
-    this._super(...arguments);
-  },
-
-  savingChanged: Ember.observer('saveSuccess', function observe() {
-    // Remove stored data when no longer saving and no errors
-    if (this.get('saveSuccess')) {
-      localStorage.removeItem(LOCALSTORAGE_KEY);
+      return val.length !== 0 && git.parse(val).valid;
     }
   }),
-
-  /**
-   * Fetches project data from localstorage or return default
-   * @property {Object} scmUrl
-   */
-  scmUrl: Ember.computed(() => {
-    let storedData = '';
-
-    try {
-      storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || storedData;
-    // eslint-disable-next-line no-empty
-    } catch (ignore) {}
-
-    return storedData;
-  }),
+  isInvalid: Ember.computed.not('isValid'),
+  isDisabled: Ember.computed.or('isSaving', 'isInvalid'),
 
   actions: {
+    /**
+     * Handles when a git url is entered in step 1
+     * @method scmChange
+     * @param  {String} val     The value of the input box
+     */
+    scmChange(val) {
+      this.set('scmUrl', val.trim());
+      const input = Ember.$('.text-input');
+
+      input.removeClass('bad-text-input good-text-input');
+
+      if (this.get('isValid')) {
+        input.addClass('good-text-input');
+      } else if (val.trim().length > 0) {
+        input.addClass('bad-text-input');
+      }
+    },
+
     /**
      * Call Api to save project
      * @event saveData
      * @param  {Object} data Project attributes
      */
     saveData() {
-      const scmUrl = this.get('scmUrl');
-
-      this.get('onCreatePipeline')(scmUrl);
-    },
-
-    /**
-     * Saves the user data if the urls are valid
-     * @event rememberProjectData
-     * @param  {Object}            data scmUrl, extConfigUrl, repoUrl
-     */
-    rememberProjectData(scmUrl) {
-      let scmUrlCheck = git.parse(scmUrl);
-
-      // validate the url data
-      if (!scmUrlCheck.valid) {
-        // invalid urls, exit
-        return;
-      }
-
-      // store scmUrl,etc data
-      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(scmUrl));
-
-      this.set('scmUrl', scmUrl);
-      this.set('currentStep', 2);
-    },
-
-    /**
-     * Updates current step
-     * @event updateStep
-     * @param  {Number}   step
-     */
-    updateStep(step) {
-      this.set('currentStep', step);
+      this.get('onCreatePipeline')(this.get('scmUrl'));
     }
   }
 });
--- tests/integration/components/pipeline-create-form/component-test.js
+++ tests/integration/components/pipeline-create-form/component-test.js
@@ -1,13 +1,8 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-const storageKey = 'create-pipeline-data';
 
 moduleForComponent('pipeline-create-form', 'Integration | Component | pipeline create form', {
-  integration: true,
-
-  beforeEach() {
-    localStorage.removeItem(storageKey);
-  }
+  integration: true
 });
 
 test('it renders', function (assert) {
@@ -15,12 +10,9 @@ test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{pipeline-create-form errorMessage="" isSaving=false saveSuccess=false}}`);
+  this.render(hbs`{{pipeline-create-form errorMessage="" isSaving=false}}`);
 
-  assert.equal($($('.step-number span')[0]).text().trim(), '1');
-  assert.equal($($('.step-number span')[1]).text().trim(), '2');
-  assert.equal($($('.step-title')[0]).text().trim(), 'Select a Repository');
-  assert.equal($($('.step-title')[1]).text().trim(), 'Create Pipeline');
+  assert.equal($('h1').text().trim(), 'Create Pipeline');
   assert.equal($('.button-label').text().trim(), 'Create Pipeline');
 });
 
@@ -28,38 +20,12 @@ test('it handles the entire ui flow', function (assert) {
   assert.expect(1);
   const scm = 'git@github.com:foo/bar.git';
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-  this.set('scmUrl', scm);
   this.set('createPipeline', (scmUrl) => {
     assert.equal(scmUrl, scm);
   });
 
   // eslint-disable-next-line max-len
-  this.render(hbs`{{pipeline-create-form errorMessage="" isSaving=false saveSuccess=false onCreatePipeline=(action createPipeline)}}`);
-  this.$('.scm-url').val(scm).keyup();
-  this.$('input.blue-button').click();
+  this.render(hbs`{{pipeline-create-form errorMessage="" isSaving=false onCreatePipeline=(action createPipeline)}}`);
+  this.$('.text-input').val(scm).keyup();
   this.$('button.blue-button').click();
-});
-
-test('it handles the entire ui flow', function (assert) {
-  assert.expect(3);
-  const scm = 'git@github.com:foo/bar.git';
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-  this.set('scmUrl', scm);
-  this.set('createPipeline', (scmUrl) => {
-    assert.equal(scmUrl, scm);
-  });
-  this.set('save', false);
-
-  // eslint-disable-next-line max-len
-  this.render(hbs`{{pipeline-create-form errorMessage="" isSaving=false saveSuccess=save onCreatePipeline=(action createPipeline)}}`);
-  this.$('.scm-url').val(scm).keyup();
-  this.$('input.blue-button').click();
-  this.$('button.blue-button').click();
-  assert.equal(JSON.parse(localStorage.getItem(storageKey)), scm);
-  this.set('save', true);
-  assert.equal(JSON.parse(localStorage.getItem(storageKey)), undefined);
 });
