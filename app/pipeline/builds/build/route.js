d7b0cfb0e28a503693d661897844dcd1e46cb196
Dao Lam <daolam112@gmail.com>
2016-12-13 16:51:18 -0800

    feat(396): Add prev and next links to build banner

--- app/pipeline/builds/build/route.js
+++ app/pipeline/builds/build/route.js
@@ -1,15 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    this.set('pipeline', this.modelFor('pipeline'));
+  },
   titleToken(model) {
     return `${model.job.get('name')} > #${model.build.get('sha').substr(0, 6)}`;
   },
   model(params) {
     return this.store.findRecord('build', params.build_id).then(build => Ember.RSVP.all([
       this.store.findRecord('job', build.get('jobId')),
-      this.store.findRecord('event', build.get('eventId'))
-    ]).then(([job, event]) => ({
-      build, job, event, pipeline: this.modelFor('pipeline')
+      this.store.findRecord('event', build.get('eventId')),
+      this.get('pipeline.jobs')
+    ]).then(([job, event, jobs]) => ({
+      build,
+      job,
+      event,
+      pipeline: this.get('pipeline'),
+      jobs: jobs.filter(j => !/^PR-/.test(j.get('name')))
     })));
   }
 });
--- tests/integration/components/build-banner/component-test.js
+++ tests/integration/components/build-banner/component-test.js
@@ -1,7 +1,8 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-const eventMock = {
+const eventMock = Ember.Object.create({
   id: 'abcd',
   causeMessage: 'Merged by batman',
   commit: {
@@ -26,8 +27,19 @@ const eventMock = {
   sha: 'abcdef1029384',
   truncatedSha: 'abcdef',
   type: 'pipeline',
-  workflow: ['main', 'publish']
-};
+  workflow: ['main', 'publish'],
+  builds: ['build1', 'build2']
+});
+
+const jobMock = [
+  Ember.Object.create({
+    id: '1',
+    name: 'main'
+  }), Ember.Object.create({
+    id: '2',
+    name: 'publish'
+  })
+];
 
 moduleForComponent('build-banner', 'Integration | Component | build banner', {
   integration: true
@@ -44,6 +56,7 @@ test('it renders', function (assert) {
   });
 
   this.set('eventMock', eventMock);
+  this.set('jobMock', jobMock);
   this.render(hbs`{{build-banner
     buildContainer="node:6"
     buildStatus="SUCCESS"
@@ -52,28 +65,30 @@ test('it renders', function (assert) {
     isAuthenticated=false
     jobName="PR-671"
     reloadBuild=(action willRender)
+    jobs=jobMock
+    eventBuilds=eventMock.builds
   }}`);
 
   assert.equal($('h1').text().trim(), 'PR-671');
-  assert.equal($('.line2 a').prop('href'),
+  assert.equal($('.commit a').prop('href'),
     'http://example.com/batcave/batmobile/commit/abcdef1029384');
-  assert.equal($('.line2 a').text().trim(), '#abcdef');
+  assert.equal($('.commit a').text().trim(), '#abcdef');
   assert.equal($('.message').text().trim(),
     'Merge it');
   assert.equal($('.message').prop('title'),
     'Merge pull request #2 from batcave/batmobile');
-  assert.equal($('.container').text().trim(), 'node:6');
-  assert.equal($('.duration').text().trim(), '5 seconds');
+  assert.equal($('.event .pure-u-1-3:nth-child(2)').text().trim(), 'node:6');
+  assert.equal($('.event .pure-u-1-3:nth-child(3)').text().trim(), '5 seconds');
   assert.equal($('button').length, 0);
 });
 
-test('it does not render a restart button for non-PR jobs when authenticated', function (assert) {
+test('it does not render a restart button for main job when authenticated', function (assert) {
   assert.expect(2);
   this.set('willRender', () => {
     assert.ok(true);
   });
-
   this.set('eventMock', eventMock);
+  this.set('jobMock', jobMock);
   this.render(hbs`{{build-banner
     buildContainer="node:6"
     buildStatus="ABORTED"
@@ -82,6 +97,30 @@ test('it does not render a restart button for non-PR jobs when authenticated', f
     isAuthenticated=true
     jobName="main"
     reloadBuild=(action willRender)
+    jobs=jobMock
+    eventBuilds=eventMock.builds
+  }}`);
+
+  assert.equal(this.$('button').length, 0);
+});
+
+test('it does not render a restart button for publish job when authenticated', function (assert) {
+  assert.expect(2);
+  this.set('willRender', () => {
+    assert.ok(true);
+  });
+  this.set('eventMock', eventMock);
+  this.set('jobMock', jobMock);
+  this.render(hbs`{{build-banner
+    buildContainer="node:6"
+    buildStatus="ABORTED"
+    duration="5 seconds"
+    event=eventMock
+    isAuthenticated=true
+    jobName="publish"
+    reloadBuild=(action willRender)
+    jobs=jobMock
+    eventBuilds=eventMock.builds
   }}`);
 
   assert.equal(this.$('button').length, 0);
@@ -98,6 +137,7 @@ test('it renders a restart button for PR jobs when authenticated', function (ass
   });
 
   this.set('eventMock', eventMock);
+  this.set('jobMock', jobMock);
   this.render(hbs`{{build-banner
     buildContainer="node:6"
     buildStatus="ABORTED"
@@ -107,6 +147,8 @@ test('it renders a restart button for PR jobs when authenticated', function (ass
     jobName="PR-671"
     reloadBuild=(action willRender)
     onStart=(action externalStart)
+    jobs=jobMock
+    eventBuilds=eventMock.builds
   }}`);
 
   assert.equal(this.$('button').text().trim(), 'Restart');
@@ -122,8 +164,8 @@ test('it renders a stop button for job when authenticated', function (assert) {
   this.set('externalStop', () => {
     assert.ok(true);
   });
-
   this.set('eventMock', eventMock);
+  this.set('jobMock', jobMock);
   this.render(hbs`{{build-banner
     buildContainer="node:6"
     buildStatus="RUNNING"
@@ -133,6 +175,8 @@ test('it renders a stop button for job when authenticated', function (assert) {
     jobName="main"
     reloadBuild=(action willRender)
     onStop=(action externalStop)
+    jobs=jobMock
+    eventBuilds=eventMock.builds
   }}`);
 
   assert.equal(this.$('button').text().trim(), 'Stop');
