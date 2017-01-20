25e2c678b5b77b0979a758800f9d84aae0661125
Peter Peterson <jedipetey@gmail.com>
2017-01-20 14:00:27 -0800

    feat: error component will display proper status code

--- app/components/error-view/component.js
+++ app/components/error-view/component.js
@@ -2,18 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   statusCode: 404,
-  errorMessage: '',
-  backgroundImage: Ember.computed('statusCode', {
-    get() {
-      let statusCode = this.get('statusCode');
-
-      let image = '500-error-page.png';
-
-      if (statusCode < 500) {
-        image = '404-error-page.png';
-      }
-
-      return image;
-    }
-  })
+  statusMessage: 'Page Not Found',
+  errorMessage: ''
 });
--- tests/integration/components/error-view/component-test.js
+++ tests/integration/components/error-view/component-test.js
@@ -6,11 +6,11 @@ moduleForComponent('error-view', 'Integration | Component | error view', {
 });
 
 test('it renders', function (assert) {
-  this.set('sc', 404);
-  this.render(hbs`{{error-view errorMessage="bananas" statusCode=sc}}`);
+  this.set('sc', 400);
+  this.set('sm', 'they are dead, dave');
+  this.render(hbs`{{error-view errorMessage="bananas" statusCode=sc statusMessage=sm}}`);
 
+  assert.equal(this.$('h1').text().trim(), '400');
+  assert.equal(this.$('h2').text().trim(), 'they are dead, dave');
   assert.equal(this.$('h4').text().trim(), 'bananas');
-  assert.ok(this.$('img').prop('src').match('/assets/404-error-page.png$'));
-  this.set('sc', 500);
-  assert.ok(this.$('img').prop('src').match('/assets/500-error-page.png$'));
 });
