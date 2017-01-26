9816c6cea15039d6fbe2c970370b2f76ab42c395
Ryosuke Nakayama <catto.je1mji@gmail.com>
2017-01-26 20:32:38 +0900

    feat: Add support for search filter

--- app/components/search-list/component.js
+++ app/components/search-list/component.js
@@ -3,11 +3,38 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   pipelineSorting: ['appId', 'branch'],
   sortedPipelines: Ember.computed.sort('pipelines', 'pipelineSorting'),
-  filteredPipelines: Ember.computed('query', 'sortedPipelines', {
+  filterSet: Ember.computed('query', {
+    get() {
+      const q = this.get('query') || '';
+      const keywords = q.split(/\s+/);
+      const filters = keywords.map((k) => {
+        let pair = k.split(/:/);
+
+        if (k.match(/[A-Za-z]+:[^ :]+/)) {
+          return { key: pair[0], value: pair[1] };
+        }
+
+        // By default, search appId by keywords
+        return { key: 'appId', value: k };
+      });
+
+      return filters;
+    }
+  }),
+  filteredPipelines: Ember.computed('sortedPipelines', 'filterSet', {
     get() {
       const pipelines = this.get('sortedPipelines');
-      const q = this.get('query') || '';
-      const filtered = pipelines.filter(p => p.get('appId').indexOf(q) > -1);
+      const filterSet = this.get('filterSet');
+      let filtered = pipelines;
+
+      filterSet.forEach((filter) => {
+        filtered = filtered.filter((p) => {
+          const field = Ember.inspect(p.get(filter.key));
+
+          // skip filtering if value is empty
+          return !filter.value || (field && field.indexOf(filter.value) > -1);
+        });
+      });
 
       return filtered;
     }
