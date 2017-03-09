65f70cb65ebd224d25034db1d1e44f15c119a1b1
Jerry Zhang <thejerryzhang@gmail.com>
2017-03-08 17:31:41 -0800

    Basic clean up

--- app/components/artifact-tree/component.js
+++ app/components/artifact-tree/component.js
@@ -1,21 +1,4 @@
 import Ember from 'ember';
-// for testing
-// const testTreedata = [
-//   {
-//     text: 'node1',
-//     type: 'folder',
-//     children: [
-//           { text: 'child1', type: 'file', a_attr: { href: 'http://www.google.com' } },
-//           { text: 'child2', type: 'file', a_attr: { href: 'http://www.google.com' } }
-//     ]
-//   },
-//   {
-//     text: 'node2',
-//     type: 'file',
-//     a_attr: { href: 'http://www.google.com' }
-//   }
-// ];
-
 const typesOptions = {
   directory: {
     icon: 'fa fa-folder-o fa-lg'
@@ -36,17 +19,11 @@ export default Ember.Component.extend({
       const buildStatus = this.get('buildStatus');
 
       if (buildStatus === 'RUNNING' || buildStatus === 'QUEUED') {
-        console.log('buildStatus running or queue: ', buildStatus);
-
         return [];
       }
 
-      console.log('buildStatus after running or queue: ', buildStatus);
-
       return this.get('artifact').fetchManifest(this.get('buildId'))
       .then((res) => {
-        console.log('res: ', res);
-
         this.set('treedata', res);
       });
     }
