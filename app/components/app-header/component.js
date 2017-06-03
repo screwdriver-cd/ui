38bec27a10b898272d4c21524bc83a5a2d89a7c1
Peter Peterson <jedipetey@gmail.com>
2017-06-02 17:48:24 -0700

    fix: responsiveness of app-header and various header styles

--- app/components/app-header/component.js
+++ app/components/app-header/component.js
@@ -4,21 +4,9 @@ export default Ember.Component.extend({
   tagName: 'header',
   showSearch: false,
 
-  /**
-   * Show/hide menu when window is small
-   * @method toggleMenu
-   */
-  toggleMenu() {
-    this.$().toggleClass('open');
-    this.$('#toggle').toggleClass('x');
-  },
-
   actions: {
     invalidateSession() {
       this.get('onInvalidate')();
-    },
-    onToggleClick() {
-      this.toggleMenu();
     },
     triggerSearch() {
       this.get('searchPipelines')(this.$('.search-input').val());
