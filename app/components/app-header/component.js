c10128d582fedbd6dac15124b38b6c5e8848e4a7
Pete Peterson <jedipetey@gmail.com>
2017-06-07 11:22:44 -0700

    feat: add alpha banner with feature flag (#165)

--- app/components/app-header/component.js
+++ app/components/app-header/component.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import ENV from 'screwdriver-ui/config/environment';
 
 export default Ember.Component.extend({
   tagName: 'header',
   showSearch: false,
-
+  showAlphaWarning: ENV.APP.FEAT_DISPLAY_ALPHA,
   actions: {
     invalidateSession() {
       this.get('onInvalidate')();
