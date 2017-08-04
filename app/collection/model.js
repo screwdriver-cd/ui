f20cdd86a3f80b18afff2ac4af73c2dbcd790581
Joel Sequeira <joelseq96@gmail.com>
2017-08-03 17:30:17 -0700

    feat(523): Add collection model and serializer

---
+++ app/collection/model.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  pipelineIds: DS.attr(),
+  pipelines: DS.attr()
+});
---
+++ tests/unit/collection/serializer-test.js
@@ -0,0 +1,86 @@
+import { moduleForModel, test } from 'ember-qunit';
+import Pretender from 'pretender';
+import Ember from 'ember';
+import wait from 'ember-test-helpers/wait';
+let server;
+
+moduleForModel('collection', 'Unit | Serializer | collection', {
+  // Specify the other units that are required for this test.
+  needs: ['serializer:collection'],
+  beforeEach() {
+    server = new Pretender();
+  },
+  afterEach() {
+    server.shutdown();
+  }
+});
+
+test('it serializes records', function (assert) {
+  let record = this.subject();
+  let serializedRecord = record.serialize();
+
+  assert.ok(serializedRecord);
+});
+
+test('it does not post with model name as key', function (assert) {
+  assert.expect(2);
+  server.post('/collections', function () {
+    return [200, {}, JSON.stringify({ collection: { id: 123 } })];
+  });
+
+  Ember.run(() => {
+    const collection = this.store().createRecord('collection', {
+      name: 'Screwdriver',
+      description: 'Collection of screwdriver pipelines'
+    });
+
+    collection.save()
+      .then(() => {
+        assert.equal(collection.get('id'), 123);
+      });
+  });
+
+  return wait().then(() => {
+    const [request] = server.handledRequests;
+    const payload = JSON.parse(request.requestBody);
+
+    assert.deepEqual(payload, {
+      name: 'Screwdriver',
+      description: 'Collection of screwdriver pipelines'
+    });
+  });
+});
+
+test('it serializes only dirty fields', function (assert) {
+  assert.expect(1);
+  server.patch('/collections/123', function () {
+    return [200, {}, JSON.stringify({ collection: { id: 123 } })];
+  });
+
+  Ember.run(() => {
+    this.store().push({
+      data: {
+        id: 123,
+        type: 'collection',
+        attributes: {
+          name: 'Screwdriver',
+          description: 'Collection of screwdriver pipelines'
+        }
+      }
+    });
+
+    const collection = this.store().peekRecord('collection', 123);
+
+    collection.set('description', 'newDescription');
+    collection.save();
+  });
+
+  return wait().then(() => {
+    const [request] = server.handledRequests;
+    const payload = JSON.parse(request.requestBody);
+
+    assert.deepEqual(payload, {
+      description: 'newDescription'
+    });
+  });
+});
