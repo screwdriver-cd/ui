b387aa20e7b6423601e696fd1fb6a5db576f3c95
catto <catto.je1mji@gmail.com>
2017-08-13 20:20:36 +0900

    feat(365): Add login buttons for each scm

--- tests/integration/components/login-button/component-test.js
+++ tests/integration/components/login-button/component-test.js
@@ -17,3 +17,23 @@ test('it renders', function (assert) {
   assert.equal(this.$('h2').text().trim(), 'Login to Screwdriver');
   this.$('a').click();
 });
+
+test('it renders multiple buttons', function (assert) {
+  const contexts = [
+    { context: 'github:github.com', displayName: 'github' },
+    { context: 'github:mygit.org', displayName: 'mygit' }
+  ];
+
+  assert.expect(5);
+  this.set('externalAction', (context) => {
+    assert.ok(context);
+  });
+  this.set('model', contexts);
+  this.render(hbs`{{login-button authenticate=(action externalAction) contexts=model}}`);
+
+  assert.equal(this.$('a').length, 2);
+  contexts.forEach((context, index) => {
+    assert.equal(this.$(`a:eq(${index})`).text(), ` Login with ${context.displayName}`);
+    this.$(`a:eq(${index})`).click();
+  });
+});
