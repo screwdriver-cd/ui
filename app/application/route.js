021b04d2f2b8819cbfedae57b0784eb29c12b27c
Jithin Emmanuel <jithin1987@gmail.com>
2017-09-19 16:34:29 -0700

    fix: Stay in current page after login/logout

--- app/application/route.js
+++ app/application/route.js
@@ -3,6 +3,18 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   routeAfterAuthentication: 'home',
+  sessionInvalidated: function sessionInvalidated() {
+    window.location.replace(window.location.href);
+  },
+  sessionAuthenticated: function sessionAuthenticated() {
+    const previousUrl = this.controllerFor('application').get('fromUrl');
+
+    if (previousUrl) {
+      this.transitionTo(previousUrl);
+    } else {
+      this._super();
+    }
+  },
   title(tokens) {
     let arr = Array.isArray(tokens) ? tokens : [];
 
--- tests/unit/application/route-test.js
+++ tests/unit/application/route-test.js
@@ -1,4 +1,5 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 
 moduleFor('route:application', 'Unit | Route | application', {
   // Specify the other units that are required for this test.
@@ -17,4 +18,32 @@ test('it calculates title', function (assert) {
   assert.equal(route.title(), 'screwdriver.cd');
   assert.equal(route.title([]), 'screwdriver.cd');
   assert.equal(route.title(['a', 'b', 'c']), 'a > b > c > screwdriver.cd');
+});
+
+test('it handles sessionInvalidated', function (assert) {
+  const route = this.subject();
+  const locationStub = this.stub(window.location, 'replace');
+
+  route.sessionInvalidated();
+
+  assert.ok(locationStub.calledOnce, 'window.location.replace should be called once');
+});
+
+test('it should do transitionTo on sessionAuthenticated when valid fromUrl', function (assert) {
+  const route = this.subject();
+  const transitionStub = this.stub(route, 'transitionTo');
+  const contollerForStub = this.stub(route, 'controllerFor');
+
+  contollerForStub.withArgs('application').returns({
+    get: (arg) => {
+      assert.equal(arg, 'fromUrl');
+
+      return 'fromUrl';
+    }
+  });
+
+  route.sessionAuthenticated();
+
+  assert.ok(transitionStub.calledOnce, 'transitionTo was not called');
+  assert.ok(transitionStub.calledWithExactly('fromUrl'), 'invalid data');
 });
