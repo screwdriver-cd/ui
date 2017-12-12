28ad5e60275588be1838e8469e9657b044bdf85c
Peter Peterson <jedipetey@gmail.com>
2017-12-12 11:33:23 -0800

    fix(803): artifact icons not displaying

--- app/components/artifact-tree/component.js
+++ app/components/artifact-tree/component.js
@@ -8,10 +8,10 @@ import ObjectProxy from '@ember/object/proxy';
 const ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
 const typesOptions = {
   directory: {
-    icon: 'fa folder-o fa-lg'
+    icon: 'fa fa-folder-o fa-lg'
   },
   file: {
-    icon: 'fa file-text-o'
+    icon: 'fa fa-file-text-o'
   }
 };
 
