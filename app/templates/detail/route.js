1308b98d700b501d9d9b2f8fc11ef811ffff6883
Philip Scott <pscott@zeptohost.com>
2018-01-30 14:35:15 -0800

    fix: Fix template page break when no tags exist

--- app/templates/detail/route.js
+++ app/templates/detail/route.js
@@ -7,17 +7,19 @@ export default Route.extend({
   model(params) {
     return RSVP.all([
       this.get('template').getOneTemplate(params.name),
-      this.get('template').getTemplateTags(params.name)
+      this.get('template').getTemplateTags(params.name).catch(err => err)
     ]).then((arr) => {
       const [verPayload, tagPayload] = arr;
 
-      tagPayload.forEach((tagObj) => {
-        const taggedVerObj = verPayload.find(verObj => verObj.version === tagObj.version);
+      if (tagPayload !== '404 Not Found') {
+        tagPayload.forEach((tagObj) => {
+          const taggedVerObj = verPayload.find(verObj => verObj.version === tagObj.version);
 
-        taggedVerObj.tag = taggedVerObj.tag ? `${taggedVerObj.tag} ${tagObj.tag}` : tagObj.tag;
-      });
+          taggedVerObj.tag = taggedVerObj.tag ? `${taggedVerObj.tag} ${tagObj.tag}` : tagObj.tag;
+        });
+      }
 
       return verPayload;
-    });
+    })
   }
 });
