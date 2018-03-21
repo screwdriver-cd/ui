5631195eb60f7ddef6f426d4dbf8ec7a2c96a93d
Peter Peterson <jedipetey@gmail.com>
2018-03-21 11:06:47 -0700

    fix: auto-open step collections when displaying their logs

--- app/components/build-step-collection/component.js
+++ app/components/build-step-collection/component.js
@@ -1,13 +1,14 @@
 import Component from '@ember/component';
-import { filter } from '@ember/object/computed';
+import { filter, mapBy } from '@ember/object/computed';
 import { get, set, computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['build-step-collection', 'row'],
   setupCollapsed: true,
-  setupSteps: filter('stepList', item => /^sd-setup/.test(item)),
+  stepNames: mapBy('buildSteps', 'name'),
+  setupSteps: filter('stepNames', item => /^sd-setup/.test(item)),
   teardownCollapsed: true,
-  teardownSteps: filter('stepList', item => /^sd-teardown/.test(item)),
+  teardownSteps: filter('stepNames', item => /^sd-teardown/.test(item)),
   selectedStep: computed('buildSteps.@each.{code,startTime,endTime}', {
     get() {
       const steps = get(this, 'buildSteps');
@@ -16,10 +17,16 @@ export default Component.extend({
       const failedStep = steps.find(s => s.code);
       const name = (runningStep && runningStep.name) || (failedStep && failedStep.name) || null;
 
+      if (name && get(this, 'setupSteps').includes(name)) {
+        set(this, 'setupCollapsed', false);
+      } else if (name && get(this, 'teardownSteps').includes(name)) {
+        set(this, 'teardownCollapsed', false);
+      }
+
       return name;
     }
   }),
-  userSteps: filter('stepList',
+  userSteps: filter('stepNames',
     item => !/^sd-setup/.test(item) && !/^sd-teardown/.test(item)),
   actions: {
     toggleSetup() {
