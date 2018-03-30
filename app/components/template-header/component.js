ed3f37612940a14ac25a87f9241bfb3f9a6833c2
Pranav Ravichandran <pranavrc@yahoo-inc.com>
2018-03-30 12:14:20 -0700

    feat(924):Template Deletion (#271)

--- app/components/template-header/component.js
+++ app/components/template-header/component.js
@@ -1,4 +1,25 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  templateToRemove: null,
+  isRemoving: false,
+  init() {
+    this._super(...arguments);
+  },
+  actions: {
+    setTemplateToRemove(template) {
+      this.set('templateToRemove', template);
+    },
+    cancelRemovingTemplate() {
+      this.set('templateToRemove', null);
+      this.set('isRemoving', false);
+    },
+    removeTemplate(name) {
+      this.set('isRemoving', true);
+      this.get('onRemoveTemplate')(name).then(() => {
+        this.set('templateToRemove', null);
+        this.set('isRemoving', false);
+      });
+    }
+  }
 });
--- app/templates/detail/controller.js
+++ app/templates/detail/controller.js
@@ -1,16 +1,22 @@
+import { inject as service } from '@ember/service';
 import { computed, observer } from '@ember/object';
 import Controller from '@ember/controller';
 const { alias } = computed;
 
 export default Controller.extend({
   selectedVersion: null,
+  errorMessage: '',
+  template: service(),
   templates: alias('model'),
+  reset: function() {
+    this.set('errorMessage', '');
+  },
   latest: computed('templates.[]', {
     get() {
       return this.get('templates')[0];
     }
   }),
-  template: computed('selectedVersion', 'templates.[]', {
+  versionTemplate: computed('selectedVersion', 'templates.[]', {
     get() {
       const version = this.get('selectedVersion') || this.get('latest.version');
 
@@ -24,6 +30,12 @@ export default Controller.extend({
   actions: {
     changeVersion(version) {
       this.set('selectedVersion', version);
+    },
+    removeTemplate(name) {
+      return this.get('template').deleteTemplates(name)
+        .then(
+          () => this.transitionToRoute('templates'),
+          (err) => this.set('errorMessage', err));
     }
   }
 });
