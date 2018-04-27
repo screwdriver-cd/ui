a11ae5db04e2a8bf9c7675bb3a26798c97d42e59
Tiffany K <tiffanykyi@gmail.com>
2018-04-27 13:41:55 -0700

    fix: Revert detached build change since it still needs other data to start (#285)

--- tests/unit/pipeline/index/controller-test.js
+++ tests/unit/pipeline/index/controller-test.js
@@ -90,14 +90,22 @@ test('it restarts a build', function (assert) {
     201,
     { 'Content-Type': 'application/json' },
     JSON.stringify({
-      id: '2',
-      pipelineId: '1234'
+      id: '2'
     })
   ]);
 
   let controller = this.subject();
 
   run(() => {
+    controller.store.push({
+      data: {
+        id: '123',
+        type: 'build',
+        attributes: {
+          parentBuildId: '345'
+        }
+      }
+    });
     controller.set('selectedEventObj', {
       id: '1',
       sha: 'sha'
@@ -132,7 +140,11 @@ test('it restarts a build', function (assert) {
 
     assert.notOk(controller.get('isShowingModal'));
     assert.deepEqual(payload, {
+      pipelineId: '1234',
+      startFrom: 'deploy',
       buildId: 123,
+      parentBuildId: 345,
+      parentEventId: 1,
       causeMessage: 'apple clicked restart for job "deploy" for sha sha'
     });
   });
