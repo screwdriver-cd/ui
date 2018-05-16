d9fbd8fd77c18262b6c9328ddf19b99670dd1e03
Russ DeLuca <russ.deluca@teamaol.com>
2018-05-16 18:54:14 -0400

    feat(966): add banner ui (#289)
    * feat(966): add banners
    
    * feat(966): banner styling

---
+++ app/components/nav-banner/component.js
@@ -0,0 +1,25 @@
+import { set } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Component from '@ember/component';
+
+export default Component.extend({
+  banner: service('banner'),
+
+  actions: {
+    clearMessage() {
+      this.set('message', null);
+    }
+  },
+
+  setBanners() {
+    this.get('banner').fetchBanners().then((banners) => {
+      set(this, 'banners', banners);
+    });
+  },
+
+  // Start loading active banners immediately upon inserting the element
+  didInsertElement() {
+    this._super(...arguments);
+    this.setBanners();
+  }
+});
---
+++ tests/unit/banner/service-test.js
@@ -0,0 +1,45 @@
+import Pretender from 'pretender';
+import { moduleFor, test } from 'ember-qunit';
+const actualMessage = 'shutdown imminent';
+let server;
+
+const getBanners = () => {
+  server.get('http://localhost:8080/v4/banners', () => [
+    200,
+    {
+      'Content-Type': 'application/json'
+    },
+    JSON.stringify([
+      { id: 1, isActive: true, message: actualMessage }
+    ])
+  ]);
+};
+
+moduleFor('service:banner', 'Unit | Service | banner', {
+  integration: true,
+
+  beforeEach() {
+    server = new Pretender();
+  },
+
+  afterEach() {
+    server.shutdown();
+  }
+});
+
+test('it exists', function (assert) {
+  const service = this.subject();
+
+  assert.ok(service);
+});
+
+test('it fetches active banners', function (assert) {
+  assert.expect(1);
+  getBanners();
+  const service = this.subject();
+  const b = service.fetchBanners();
+
+  b.then((banners) => {
+    assert.equal(banners[0].message, actualMessage);
+  });
+});
