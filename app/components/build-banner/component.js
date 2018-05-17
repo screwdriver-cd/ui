8c2e55d5ad802f1658236be1e5e061f6007eda5b
Min Zhang <minzhangcmu@gmail.com>
2018-05-16 19:08:04 -0700

    fix: do not keep recomputing coverage info

--- app/components/build-banner/component.js
+++ app/components/build-banner/component.js
@@ -1,50 +1,26 @@
 import { computed } from '@ember/object';
-import { match, filter } from '@ember/object/computed';
+import { alias, match } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
-import ObjectProxy from '@ember/object/proxy';
-
-const ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
 
 export default Component.extend({
   classNames: ['build-banner', 'row'],
   classNameBindings: ['buildStatus'],
   coverage: service(),
   isPR: match('jobName', /^PR-/),
-  coverageStep: filter('buildSteps', item => /^sd-teardown-screwdriver-coverage/.test(item.name)),
-
-  coverageInfo: computed('coverageStep', {
+  coverageStep: computed('buildSteps', {
     get() {
-      const coverageStep = this.get('coverageStep');
+      const buildSteps = this.get('buildSteps');
+      const coverageStep = buildSteps.find(item =>
+        /^sd-teardown-screwdriver-coverage/.test(item.name));
 
-      // No coverage step, return empty object
-      if (!coverageStep || coverageStep.length <= 0) {
-        return {};
-      }
-
-      const coverageStepData = coverageStep.objectAt(0);
-
-      // Coverage step not finished yet, return place holder value
-      if (!coverageStepData.endTime) {
-        return {
-          coverage: 'N/A',
-          projectUrl: '#'
-        };
-      }
-
-      const config = {
-        buildId: this.get('buildId'),
-        jobId: this.get('jobId'),
-        startTime: coverageStepData.startTime,
-        endTime: coverageStepData.endTime
-      };
-
-      return ObjectPromiseProxy.create({
-        promise: this.get('coverage').getCoverageInfo(config)
-      });
+      return coverageStep;
     }
   }),
+
+  coverageStepEndTime: alias('coverageStep.endTime'),
+
+  coverageStepStartTime: alias('coverageStep.startTime'),
 
   buildAction: computed('buildStatus', {
     get() {
@@ -78,6 +54,41 @@ export default Component.extend({
     }
   }),
 
+  coverageInfoCompute() {
+    const coverageStepEndTime = this.get('coverageStepEndTime');
+    const coverageStepStartTime = this.get('coverageStepStartTime');
+
+    if (!coverageStepEndTime) {
+      this.set('coverageInfo', {
+        projectUrl: '#',
+        coverage: 'N/A'
+      });
+
+      return;
+    }
+
+    const config = {
+      buildId: this.get('buildId'),
+      jobId: this.get('jobId'),
+      startTime: coverageStepStartTime,
+      endTime: coverageStepEndTime
+    };
+
+    this.get('coverage').getCoverageInfo(config)
+      .then((data) => {
+        this.set('coverageInfo', data);
+        this.set('coverageInfoSet', true);
+      });
+  },
+
+  init() {
+    this._super(...arguments);
+
+    this.set('coverageInfoSet', false);
+
+    this.coverageInfoCompute();
+  },
+
   willRender() {
     this._super(...arguments);
 
@@ -85,6 +96,10 @@ export default Component.extend({
 
     if (status === 'QUEUED' || status === 'RUNNING') {
       this.get('reloadBuild')();
+    }
+
+    if (this.get('coverageStepEndTime') && !this.get('coverageInfoSet')) {
+      this.coverageInfoCompute();
     }
   },
 
