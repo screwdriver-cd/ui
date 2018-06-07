e7486a1b34b841aaa21dd8afff28b29a5395ecda
Peter Peterson <jedipetey@gmail.com>
2018-06-07 10:33:37 -0700

    update to ember 3.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,8 +35,9 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
         'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
