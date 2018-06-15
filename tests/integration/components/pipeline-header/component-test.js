b48aaaf45b51e228911918fc0464066973659647
Filbird <filidillidally@gmail.com>
2018-06-14 18:46:20 -0700

    feat(937): Add link to parent on child pipeline header

--- tests/integration/components/pipeline-header/component-test.js
+++ tests/integration/components/pipeline-header/component-test.js
@@ -32,3 +32,27 @@ test('it renders', function (assert) {
   assert.equal($('span.scm').text().trim(), 'github.com');
   assert.equal($('.scm > .fa-github').length, 1);
 });
+
+test('it renders link to parent pipeline for child pipeline', function (assert) {
+  const $ = this.$;
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  const pipelineMock = EmberObject.create({
+    appId: 'batman/batmobile',
+    hubUrl: 'http://example.com/batman/batmobile',
+    branch: 'master',
+    scmContext: 'github.com',
+    configPipelineId: '123'
+  });
+  const scmMock = EmberObject.create({
+    scm: 'github.com',
+    scmIcon: 'github'
+  });
+
+  this.set('pipelineMock', pipelineMock);
+  this.set('scmMock', scmMock);
+  this.render(hbs`{{pipeline-header pipeline=pipelineMock scmContext=scmMock}}`);
+
+  assert.equal($($('a').get(2)).text().trim(), 'Back to parent pipeline');
+});
