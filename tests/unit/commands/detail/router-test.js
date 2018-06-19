40ef6a8f380221fcb2a88443088c0485e5661cd9
Tiffany Kyi <tiffanykyi@gmail.com>
2018-06-19 14:04:04 -0700

    fix: Add tests and last updated for command

--- tests/unit/commands/detail/router-test.js
+++ tests/unit/commands/detail/router-test.js
@@ -2,12 +2,13 @@ import { resolve } from 'rsvp';
 import Service from '@ember/service';
 import { moduleFor, test } from 'ember-qunit';
 
+const createTime = '2016-09-23T16:53:00.274Z';
 const commandServiceStub = Service.extend({
   getOneCommand(namespace, name) {
     return resolve([
-      { id: 3, namespace, name, version: '3.0.0' },
-      { id: 2, namespace, name, version: '2.0.0' },
-      { id: 1, namespace, name, version: '1.0.0' }
+      { id: 3, namespace, name, version: '3.0.0', createTime },
+      { id: 2, namespace, name, version: '2.0.0', createTime },
+      { id: 1, namespace, name, version: '1.0.0', createTime }
     ]);
   }
 });
@@ -22,11 +23,14 @@ moduleFor('route:commands/detail', 'Unit | Route | commands/detail', {
 
 test('it asks for the list of commands for a given namespace and name', function (assert) {
   let route = this.subject();
+  const commandCreateTime = new Date(createTime).getTime();
 
   assert.ok(route);
 
   return route.model({ namespace: 'foo', name: 'bar' }).then((commands) => {
     assert.equal(commands[0].name, 'bar');
     assert.equal(commands[0].namespace, 'foo');
+    assert.equal(commands[0].lastUpdated,
+      `${humanizeDuration(Date.now() - commandCreateTime, { round: true, largest: 1 })} ago`);
   });
 });
--- tests/unit/templates/index/route-test.js
+++ tests/unit/templates/index/route-test.js
@@ -2,12 +2,13 @@ import { resolve } from 'rsvp';
 import Service from '@ember/service';
 import { moduleFor, test } from 'ember-qunit';
 
+const createTime = '2016-09-23T16:53:00.274Z';
 const templateServiceStub = Service.extend({
   getAllTemplates() {
     return resolve([
-      { id: 3, name: 'foo/bar', version: '3.0.0' },
-      { id: 2, name: 'foo/bar', version: '2.0.0' },
-      { id: 1, name: 'bar/baz', version: '1.0.0' }
+      { id: 3, name: 'foo/bar', version: '3.0.0', createTime },
+      { id: 2, name: 'foo/bar', version: '2.0.0', createTime },
+      { id: 1, name: 'bar/baz', version: '1.0.0', createTime }
     ]);
   }
 });
@@ -22,10 +23,13 @@ moduleFor('route:templates/index', 'Unit | Route | templates/index', {
 
 test('it dedupes the templates by name', function (assert) {
   let route = this.subject();
+  const templateCreateTime = new Date(createTime).getTime();
 
   assert.ok(route);
 
   return route.model().then((templates) => {
     assert.equal(templates.length, 2);
+    assert.equal(templates[0].lastUpdated,
+      `${humanizeDuration(Date.now() - templateCreateTime, { round: true, largest: 1 })} ago`);
   });
 });
