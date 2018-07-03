d07de307a9071260cefe31524f99785d5d56a920
Jithin Emmanuel <jithin1987@gmail.com>
2018-07-03 10:33:17 -0700

    fix: Show Stop for UNSTABLE running builds (#327)
    * fix: Show Stop for UNSTABLE running builds
    
    * fix: template to check for object existence
    
    * fix: command to check for object existence

--- app/utils/build.js
+++ app/utils/build.js
@@ -1,9 +1,11 @@
 /**
  * Check if the current build is active based on build status
- * @param  {String}  status Build status
+ * @param  {String}  status  Build status
+ * @param  {String}  endTime Build end time
  * @return {Boolean} true if build is active.
  */
-const isActiveBuild = status => status === 'QUEUED' || status === 'RUNNING' || status === 'BLOCKED';
+const isActiveBuild = (status, endTime) => status === 'QUEUED' || status === 'RUNNING'
+    || status === 'BLOCKED' || (status === 'UNSTABLE' && !endTime);
 
 /**
  * Check if the current job is a PR job
