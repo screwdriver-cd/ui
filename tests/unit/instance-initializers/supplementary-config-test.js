6d3655f02c493f20ad582e6e44b8f5ed641a8c52
Dekus <dekusdenial@hotmail.com>
2018-09-24 19:27:47 -0400

    feat(1283): custom doc link (#362)

--- tests/unit/instance-initializers/supplementary-config-test.js
+++ tests/unit/instance-initializers/supplementary-config-test.js
@@ -6,6 +6,7 @@ import ENV from 'screwdriver-ui/config/environment';
 import destroyApp from '../../helpers/destroy-app';
 const NAMESPACE = ENV.APP.SDAPI_NAMESPACE;
 const HOSTNAME = ENV.APP.SDAPI_HOSTNAME;
+const SDDOC_URL = ENV.APP.SDDOC_URL;
 
 module('Unit | Instance Initializer | supplementary config', {
   beforeEach() {
@@ -21,6 +22,7 @@ module('Unit | Instance Initializer | supplementary config', {
     delete window.SUPPLEMENTARY_CONFIG;
     ENV.APP.SDAPI_NAMESPACE = NAMESPACE;
     ENV.APP.SDAPI_HOSTNAME = HOSTNAME;
+    ENV.APP.SDDOC_URL = SDDOC_URL;
   }
 });
 
@@ -29,16 +31,19 @@ test('it uses the pre-configured settings', function (assert) {
 
   assert.equal(ENV.APP.SDAPI_NAMESPACE, NAMESPACE);
   assert.equal(ENV.APP.SDAPI_HOSTNAME, HOSTNAME);
+  assert.equal(ENV.APP.SDDOC_URL, SDDOC_URL);
 });
 
 test('it uses the supplementary-config settings', function (assert) {
   window.SUPPLEMENTARY_CONFIG = {
     SDAPI_NAMESPACE: 'v5',
-    SDAPI_HOSTNAME: 'http://example.com'
+    SDAPI_HOSTNAME: 'http://example.com',
+    SDDOC_URL: 'http://custom.doc'
   };
 
   initialize(this.appInstance);
 
   assert.equal(ENV.APP.SDAPI_NAMESPACE, 'v5');
   assert.equal(ENV.APP.SDAPI_HOSTNAME, 'http://example.com');
+  assert.equal(ENV.APP.SDDOC_URL, 'http://custom.doc');
 });
