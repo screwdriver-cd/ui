2bb6c8d086375c5ca0642c33909cf2257dcf4cbe
Alan Dong <adong@oath.com>
2019-06-13 12:44:47 -0700

    Fix typo change showCollection to showCollections for plural case

--- app/application/controller.js
+++ app/application/controller.js
@@ -6,7 +6,7 @@ const { alias } = computed;
 export default Controller.extend({
   session: service('session'),
   scmContexts: alias('model'),
-  showCollection: false,
+  showCollections: false,
   actions: {
     invalidateSession() {
       this.session.set('data.sessionChanged', false);
