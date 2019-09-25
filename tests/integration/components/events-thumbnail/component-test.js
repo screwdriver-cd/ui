5ca90d22c1f3426dc99bfdb635b389dfeda4e62d
code-beast <evan.gaoyf@gmail.com>
2019-09-25 08:13:27 -0700

    Added tests for events-thumbnail component

--- tests/integration/components/events-thumbnail/component-test.js
+++ tests/integration/components/events-thumbnail/component-test.js
@@ -2,25 +2,39 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import $ from 'jquery';
 
-module('Integration | Component | events-thumbnail', function(hooks) {
+const eventsInfo = [
+  {
+    statusColor: 'build-success',
+    duration: 10
+  },
+  {
+    statusColor: 'build-failure',
+    duration: 20
+  }
+];
+const BUILD_SUCCESS_COLOR = 'rgb(26, 197, 103)';
+const BUILD_FAILURE_COLOR = 'rgb(220, 20, 45)';
+const BUILD_EMPTY_COLOR = 'rgb(232, 232, 232)';
+
+module('Integration | Component | events thumbnail', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('eventsInfo', eventsInfo);
 
-    await render(hbs`{{events-thumbnail}}`);
+    await render(hbs`{{events-thumbnail events=eventsInfo}}`);
+    assert.dom('svg').exists({ count: 1 });
+    assert.dom('svg rect').exists({ count: 20 });
 
-    assert.equal(this.element.textContent.trim(), '');
+    const rect1 = $('svg rect:nth-of-type(1)');
+    const rect2 = $('svg rect:nth-of-type(2)');
 
-    // Template block usage:
-    await render(hbs`
-      {{#events-thumbnail}}
-        template block text
-      {{/events-thumbnail}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(rect1.css('fill'), BUILD_SUCCESS_COLOR);
+    assert.equal(rect1.css('height'), '20px');
+    assert.equal(rect2.css('fill'), BUILD_FAILURE_COLOR);
+    assert.equal(rect2.css('height'), '40px');
+    assert.equal($('svg rect:nth-of-type(3)').css('fill'), BUILD_EMPTY_COLOR);
   });
 });
