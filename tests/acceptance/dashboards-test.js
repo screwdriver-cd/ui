ccd98c7e55bb60244cc3877170c00ed53fa7590f
code-beast <evan.gaoyf@gmail.com>
2019-09-25 03:56:06 -0700

    Fixed all acceptance tests

--- tests/acceptance/dashboards-test.js
+++ tests/acceptance/dashboards-test.js
@@ -6,12 +6,15 @@ import Pretender from 'pretender';
 
 import {
   hasCollections,
-  hasEmptyCollections,
-  hasCollection
+  hasCollection,
+  hasDefaultCollection,
+  hasEmptyDefaultCollection,
+  hasEmptyCollection
 } from 'screwdriver-ui/tests/mock/collections';
 import { hasPipelines } from 'screwdriver-ui/tests/mock/pipeline';
 
 let server;
+const hasEmptyEvents = () => [200, { 'Content-Type': 'application/json' }, JSON.stringify([])];
 
 module('Acceptance | dashboards', function(hooks) {
   setupApplicationTest(hooks);
@@ -20,8 +23,11 @@ module('Acceptance | dashboards', function(hooks) {
     server = new Pretender();
 
     server.get('http://localhost:8080/v4/collections', hasCollections);
-    server.get('http://localhost:8080/v4/collections/1', hasCollection);
+    server.get('http://localhost:8080/v4/collections/1', hasDefaultCollection);
+    server.get('http://localhost:8080/v4/collections/2', hasCollection);
     server.get('http://localhost:8080/v4/pipelines', hasPipelines);
+    server.get('http://localhost:8080/v4/pipelines/12742/events', hasEmptyEvents);
+    server.get('http://localhost:8080/v4/pipelines/12743/events', hasEmptyEvents);
   });
 
   hooks.afterEach(function() {
@@ -34,28 +40,27 @@ module('Acceptance | dashboards', function(hooks) {
     assert.equal(currentURL(), '/login');
   });
 
-  test('visiting / when logged in and no collections', async function(assert) {
-    server.get('http://localhost:8080/v4/collections', hasEmptyCollections);
-
-    await authenticateSession({ token: 'fakeToken' });
-    await visit('/');
-    assert.equal(currentURL(), '/');
-  });
-
-  test('visiting / when logged in and have collections', async function(assert) {
-    server.get('http://localhost:8080/v4/collections', hasCollections);
+  test('visiting / when logged in and have collections among which the default collection is empty', async function(assert) {
+    server.get('http://localhost:8080/v4/collections/1', hasEmptyDefaultCollection);
     await authenticateSession({ token: 'fakeToken' });
     await visit('/');
 
     assert.equal(currentURL(), '/dashboards/1');
-    assert.dom('.header__name').hasText('collection1');
-    assert.dom('.header__description').hasText('description1');
-    assert.dom('table').exists({ count: 1 });
-    assert.dom('th.app-id').hasText('Name');
-    assert.dom('th.branch').hasText('Branch');
-    assert.dom('th.account').hasText('Account');
-    assert.dom('tr').exists({ count: 4 });
-    assert.dom('td').exists({ count: 14 });
+    assert.dom('.header__name').hasText('My Pipelines');
+    assert.dom('.header__description').hasText('default collection description');
+    assert.dom('.collection-empty-view').exists({ count: 1 });
+  });
+
+  test('visiting / when logged in and have collections among which the default collection is not empty', async function(assert) {
+    await authenticateSession({ token: 'fakeToken' });
+    await visit('/');
+
+    assert.equal(currentURL(), '/dashboards/1');
+    assert.dom('.header__name').hasText('My Pipelines');
+    assert.dom('.header__description').hasText('default collection description');
+    assert.dom('.collection-operation').exists({ count: 4 });
+    assert.dom('.collection-card-view').exists({ count: 1 });
+    assert.dom('.pipeline-card').exists({ count: 2 });
   });
 
   test('visiting /dashboards when not logged in', async function(assert) {
@@ -64,38 +69,39 @@ module('Acceptance | dashboards', function(hooks) {
     assert.equal(currentURL(), '/login');
   });
 
-  test('visiting /dashboards when logged in and no collections', async function(assert) {
-    server.get('http://localhost:8080/v4/collections', hasEmptyCollections);
+  test('visiting /dashboards/2 when collection 2 is empty', async function(assert) {
+    server.get('http://localhost:8080/v4/collections/2', hasEmptyCollection);
     await authenticateSession({ token: 'fakeToken' });
-    await visit('/dashboards');
+    await visit('/dashboards/2');
 
-    assert.equal(currentURL(), '/search');
-  });
-
-  test('visiting /dashboards/1', async function(assert) {
-    await authenticateSession({ token: 'fakeToken' });
-    await visit('/dashboards/1');
-
-    assert.equal(currentURL(), '/dashboards/1');
+    assert.equal(currentURL(), '/dashboards/2');
     assert.dom('.header__name').hasText('collection1');
     assert.dom('.header__description').hasText('description1');
-    assert.dom('table').exists({ count: 1 });
-    assert.dom('th.app-id').hasText('Name');
-    assert.dom('th.branch').hasText('Branch');
-    assert.dom('th.account').hasText('Account');
-    assert.dom('tr').exists({ count: 4 });
-    assert.dom('td').exists({ count: 14 });
+    assert.dom('.collection-empty-view').exists({ count: 1 });
+  });
+
+  test('visiting /dashboards/2 when collection 2 is not empty', async function(assert) {
+    await authenticateSession({ token: 'fakeToken' });
+    await visit('/dashboards/2');
+
+    assert.equal(currentURL(), '/dashboards/2');
+    assert.dom('.header__name').hasText('collection1');
+    assert.dom('.header__description').hasText('description1');
+    assert.dom('.collection-operation').exists({ count: 4 });
+    assert.dom('.collection-card-view').exists({ count: 1 });
+    assert.dom('.pipeline-card').exists({ count: 2 });
   });
 
   test('creating a collection', async function(assert) {
     assert.expect(7);
 
     const expectedRequestBody = {
-      name: 'collection3',
-      description: 'description3'
+      name: 'collection2',
+      description: 'description2',
+      type: 'normal'
     };
 
-    server.get('http://localhost:8080/v4/collections', hasEmptyCollections);
+    server.get('http://localhost:8080/v4/collections', hasCollections);
     server.post('http://localhost:8080/v4/collections', request => {
       assert.deepEqual(JSON.parse(request.requestBody), expectedRequestBody);
 
@@ -104,8 +110,8 @@ module('Acceptance | dashboards', function(hooks) {
         { 'Content-Type': 'application/json' },
         JSON.stringify({
           id: 3,
-          name: 'collection3',
-          description: 'description3'
+          name: 'collection2',
+          description: 'description2'
         })
       ];
     });
@@ -116,7 +122,7 @@ module('Acceptance | dashboards', function(hooks) {
     await authenticateSession({ token: 'fakeToken' });
     await visit('/');
     // Logged in but no collections, url should be `/`
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/dashboards/1');
 
     await visit('/search');
     assert.dom('.flyout').exists({ count: 1 });
@@ -125,9 +131,9 @@ module('Acceptance | dashboards', function(hooks) {
 
     await click('.new');
     assert.dom('.modal').exists({ count: 1 });
-    await fillIn('.name input', 'collection3');
+    await fillIn('.name input', 'collection2');
     await triggerEvent('.name input', 'keyup');
-    await fillIn('.description textarea', 'description3');
+    await fillIn('.description textarea', 'description2');
     await triggerEvent('.description textarea', 'keyup');
     await click('.collection-form__create');
     // The modal should disappear
