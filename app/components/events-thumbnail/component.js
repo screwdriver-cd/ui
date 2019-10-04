a90f7c8af6e269f89116084d4035a2dcb76be426
code-beast <evan.gaoyf@gmail.com>
2019-10-03 17:26:35 -0700

    Added a resize functionality to events-thumb-nail so that it is responsive to window resize

--- app/components/events-thumbnail/component.js
+++ app/components/events-thumbnail/component.js
@@ -8,9 +8,22 @@ const RANDOM_NUMS = new Array(MAX_NUM_EVENTS_SHOWN).fill(0).map(() => Math.rando
 export default Component.extend({
   store: service(),
   events: null,
+  pipelineId: undefined,
 
   didInsertElement() {
+    const getParameters = svg => {
+      const chartSize = parseInt(svg.style('width'), 10);
+      const barSpace = (chartSize * 0.96) / MAX_NUM_EVENTS_SHOWN;
+      const barWidth = (barSpace * 2) / 3;
+      const barInterval = barSpace - barWidth;
+      const paddingLeft = chartSize * 0.02 - barInterval / 2;
+      const paddingRight = chartSize * 0.02 + barInterval / 2;
+
+      return [barSpace, barWidth, paddingLeft, paddingRight];
+    };
+
     const svg = select(this.element.getElementsByTagName('svg')[0]);
+    let [barSpace, barWidth, paddingLeft, paddingRight] = getParameters(svg);
     const totalNumberOfEvents = this.events.length;
     const maxEvent = totalNumberOfEvents
       ? this.events.sort((e1, e2) => e2.duration - e1.duration)[0]
@@ -20,6 +33,9 @@ export default Component.extend({
       .scaleLinear()
       .domain([0, maxDuration])
       .range([0, 40]);
+
+    // Add padding to the svg element so that it stays symmetrical
+    svg.style('padding', `0 ${paddingLeft} 0 ${paddingRight}`);
 
     // Fixed number of bars
     this.events = this.events.reverse().concat(
@@ -36,13 +52,23 @@ export default Component.extend({
       .data(this.events)
       .enter()
       .append('rect')
-      .attr('width', 10)
+      .attr('width', barWidth)
       .attr('height', event => y(event.duration))
-      .attr('x', (build, idx) => 15 * idx)
+      .attr('x', (build, idx) => barSpace * idx)
       .attr('y', event => 40 - y(event.duration));
 
     bars.each(function addColor(bar) {
       this.classList.add(bar.statusColor.toLowerCase());
+    });
+
+    d3.select(window).on(`resize.${this.pipelineId}`, function resize() {
+      [barSpace, barWidth, paddingLeft, paddingRight] = getParameters(svg);
+
+      // change padding to the svg element so that it stays symmetrical
+      svg.style('padding', `0 ${paddingLeft} 0 ${paddingRight}`);
+
+      // change bar width and spacing to keep the thumbnail responsive
+      bars.attr('width', barWidth).attr('x', (build, idx) => barSpace * idx);
     });
   }
 });
