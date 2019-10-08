4f29533466e7c38d08f6c9a79ee89bf13f371310
Jithin Emmanuel <jithin1987@gmail.com>
2019-10-07 17:37:31 -0700

    fix: invert check for normal collection

--- app/components/collections-flyout/component.js
+++ app/components/collections-flyout/component.js
@@ -24,12 +24,13 @@ export default Component.extend({
   orderedCollections: computed('collections.[]', {
     get() {
       let defaultCollection;
-      const normalCollections = this.collections.filter(collection => {
+      const 
+      Collections = this.collections.filter(collection => {
         if (collection.type === 'default') {
           defaultCollection = collection;
         }
 
-        return collection.type === 'normal';
+        return collection.type !== 'default';
       });
 
       return defaultCollection ? [defaultCollection, ...normalCollections] : normalCollections;
