a8addd297c34893be4c3e7581fb69639043c3246
Alan Dong <alandong@stanford.edu>
2019-10-17 18:56:10 -0700

    fix: use Ember.RSVP to handle promise and catch errors

--- app/dashboard/show/route.js
+++ app/dashboard/show/route.js
@@ -1,24 +1,19 @@
 import RSVP from 'rsvp';
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
 const MAX_NUM_EVENTS_SHOWN = 20;
 
 export default Route.extend(AuthenticatedRouteMixin, {
   model(params) {
     return this.store.findRecord('collection', params.collection_id).then(collection => {
       return RSVP.hash({
-        metricsMap: RSVP.hash(
-          (collection.pipelineIds || []).reduce((oldMap, pipelineId) => {
-            oldMap[pipelineId] = this.store.query('metric', {
-              pipelineId,
-              page: 1,
-              count: MAX_NUM_EVENTS_SHOWN
-            });
-
-            return oldMap;
-          }, {})
-        ),
+        metricsMap: collection.pipelineIds.map(pipelineId => {
+          return this.store.query('metric', {
+            pipelineId,
+            page: 1,
+            count: MAX_NUM_EVENTS_SHOWN
+          });
+        }),
         collection,
         collections: this.store.findAll('collection').catch(() => [])
       });
