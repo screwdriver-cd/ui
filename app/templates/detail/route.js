111fedaea6fd12724d1123bbf6dd78be4b7692ac
Kenta Kawaharada <kkawahar@yahoo-corp.jp>
2019-12-20 19:40:18 +0900

    also fix templates

--- app/templates/detail/route.js
+++ app/templates/detail/route.js
@@ -10,13 +10,38 @@ export default Route.extend({
       this.template.getTemplateTags(params.namespace, params.name)
     ]).then(arr => {
       let [verPayload, tagPayload] = arr;
+      let version;
 
       if (params.version) {
-        const versionExists = verPayload.filter(t => t.version === params.version);
+        const versionExists = verPayload.filter(t => t.version.startsWith(params.version));
         const tagExists = tagPayload.filter(t => t.tag === params.version);
 
         if (tagExists.length === 0 && versionExists.length === 0) {
           this.transitionTo('/404');
+        }
+
+        if (versionExists.length > 0) {
+          // Sort commands by descending order
+          versionExists.sort((a, b) => {
+            const as = a.version.split('.');
+            const bs = b.version.split('.');
+            let ai;
+            let bi;
+            let limit = Math.max(as.length, bs.length);
+
+            while (limit) {
+              limit -= 1;
+              ai = parseInt(as.shift() || 0, 10);
+              bi = parseInt(bs.shift() || 0, 10);
+              if (ai !== bi) {
+                break;
+              }
+            }
+
+            return ai > bi;
+          });
+
+          ({ version } = versionExists[0]);
         }
       }
 
@@ -33,7 +58,7 @@ export default Route.extend({
       let result = {};
 
       result.templateData = verPayload;
-      result.versionOrTagFromUrl = params.version;
+      result.versionOrTagFromUrl = version || params.version;
       result.templateTagData = tagPayload;
 
       return result;
--- tests/unit/templates/detail/route-test.js
+++ tests/unit/templates/detail/route-test.js
@@ -7,12 +7,13 @@ import sinonTest from 'ember-sinon-qunit/test-support/test';
 const templateServiceStub = Service.extend({
   getOneTemplate() {
     return resolve([
-      { id: 3, name: 'baz', version: '3.0.0', namespace: 'foo' },
-      { id: 2, name: 'baz', version: '2.0.0', namespace: 'foo' },
+      { id: 4, name: 'baz', version: '3.0.0', namespace: 'foo' },
+      { id: 3, name: 'baz', version: '2.0.0', namespace: 'foo' },
+      { id: 2, name: 'baz', version: '1.1.0', namespace: 'foo' },
       { id: 1, name: 'baz', version: '1.0.0', namespace: 'foo' },
-      { id: 6, name: 'baz', version: '3.0.0', namespace: 'bar' },
-      { id: 5, name: 'baz', version: '2.0.0', namespace: 'bar' },
-      { id: 4, name: 'baz', version: '1.0.0', namespace: 'bar' }
+      { id: 7, name: 'baz', version: '3.0.0', namespace: 'bar' },
+      { id: 6, name: 'baz', version: '2.0.0', namespace: 'bar' },
+      { id: 5, name: 'baz', version: '1.0.0', namespace: 'bar' }
     ]);
   },
   getTemplateTags(namespace, name) {
@@ -39,7 +40,7 @@ module('Unit | Route | templates/detail', function(hooks) {
     assert.ok(route);
 
     return route.model({ namespace: 'foo', name: 'baz' }).then(templates => {
-      assert.equal(templates.templateData.length, 3);
+      assert.equal(templates.templateData.length, 4);
       assert.equal(templates.templateData[0].namespace, 'foo');
       assert.equal(templates.templateData[0].name, 'baz');
       assert.equal(templates.versionOrTagFromUrl, undefined);
@@ -52,10 +53,23 @@ module('Unit | Route | templates/detail', function(hooks) {
     assert.ok(route);
 
     return route.model({ namespace: 'foo', name: 'baz', version: '1.0.0' }).then(templates => {
-      assert.equal(templates.templateData.length, 3);
+      assert.equal(templates.templateData.length, 4);
       assert.equal(templates.templateData[0].namespace, 'foo');
       assert.equal(templates.templateData[0].name, 'baz');
       assert.equal(templates.versionOrTagFromUrl, '1.0.0');
+    });
+  });
+
+  test('it asks for the list of templates for a given name and exist version of according to ember', function(assert) {
+    let route = this.owner.lookup('route:templates/detail');
+
+    assert.ok(route);
+
+    return route.model({ namespace: 'foo', name: 'baz', version: '1' }).then(templates => {
+      assert.equal(templates.templateData.length, 4);
+      assert.equal(templates.templateData[0].namespace, 'foo');
+      assert.equal(templates.templateData[0].name, 'baz');
+      assert.equal(templates.versionOrTagFromUrl, '1.1.0');
     });
   });
 
@@ -65,7 +79,7 @@ module('Unit | Route | templates/detail', function(hooks) {
     assert.ok(route);
 
     return route.model({ namespace: 'foo', name: 'baz', version: 'stable' }).then(templates => {
-      assert.equal(templates.templateData.length, 3);
+      assert.equal(templates.templateData.length, 4);
       assert.equal(templates.templateData[0].namespace, 'foo');
       assert.equal(templates.templateData[0].name, 'baz');
       assert.equal(templates.versionOrTagFromUrl, 'stable');
