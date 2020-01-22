85e9677c1ba62d55190e65bfdfba914afa7b66c2
Kevin Lu <klu13960@gmail.com>
2020-01-21 16:35:08 -0800

    moved api call to route

--- app/pipeline/job-latest-build/route.js
+++ app/pipeline/job-latest-build/route.js
@@ -1,8 +1,58 @@
+import $ from 'jquery';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { Promise as EmberPromise } from 'rsvp';
+import ENV from 'screwdriver-ui/config/environment';
+
+/**
+ * Get latest build for given job
+ * @method getLatestBuild
+ * @param   {object}  session
+ * @param   {Number}  pipelineId
+ * @param   {string}  jobName
+ * @param   {string}  buildStatus
+ * @return  {Promise}            Resolve nothing if success otherwise reject with error message
+ */
+function getLatestBuild(session, pipelineId, jobName, buildStatus) {
+  let url = `${ENV.APP.SDAPI_HOSTNAME}/${ENV.APP.SDAPI_NAMESPACE}/pipelines/${pipelineId}/jobs/${jobName}/latestBuild`;
+
+  if (buildStatus) {
+    url = `${ENV.APP.SDAPI_HOSTNAME}/${ENV.APP.SDAPI_NAMESPACE}/pipelines/${pipelineId}/jobs/${jobName}/latestBuild?status=${buildStatus}`;
+  }
+
+  return new EmberPromise((resolve, reject) => {
+    if (!session.isAuthenticated) {
+      return reject(new Error('User is not authenticated'));
+    }
+
+    return $.ajax({
+      url,
+      type: 'GET',
+      headers: {
+        Authorization: `Bearer ${session.data.authenticated.token}`
+      }
+    })
+      .done(data => {
+        // setting pipelineId because build meta data is not a gurantee
+        data.pipelineId = pipelineId;
+
+        return resolve(data);
+      })
+      .fail(response => {
+        let message = `${response.status} Request Failed`;
+
+        if (response && response.responseJSON && typeof response.responseJSON === 'object') {
+          message = `${response.status} ${response.responseJSON.message}`;
+        }
+
+        return reject(message);
+      });
+  });
+}
 
 export default Route.extend({
   latestBuildService: service('build-latest'),
+  session: service('session'),
   queryParams: {
     status: ''
   },
@@ -11,6 +61,6 @@ export default Route.extend({
     const { job_name: jobName, status: buildStatus } = params;
 
     // todo: move build-latest service to this file
-    return this.latestBuildService.getLatestBuild(pipelineId, jobName, buildStatus);
+    return getLatestBuild(this.get('session'), pipelineId, jobName, buildStatus);
   }
 });
--- tests/unit/pipeline/job-latest-build/route-test.js
+++ tests/unit/pipeline/job-latest-build/route-test.js
@@ -9,4 +9,24 @@ module('Unit | Route | pipeline/job-latest-build', function(hooks) {
 
     assert.ok(route);
   });
+
+  // sinonTest('it fetches latest build for given job', function (assert) {
+  //   const route = this.owner.lookup('route:pipeline/job-latest-build');
+  //   const stub = this.stub(route, 'transitionTo');
+  //   const jobId = 345;
+  //   const pipelineId = 123;
+  //   const model = {
+  //     pipeline: {
+  //       get: type => (type === 'id' ? pipelineId : null)
+  //     },
+  //     job: {
+  //       get: type => (type === 'id' ? jobId : null)
+  //     }
+  //   };
+
+  //   route.afterModel(model);
+
+  //   assert.ok(stub.calledOnce, 'transitionTo was called once');
+  //   assert.ok(stub.calledWithExactly('pipeline', pipelineId), 'transition to pipeline');
+  // });
 });
