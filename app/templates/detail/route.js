b82d97165f845f79a809c7b4ca411511c9ef161e
InderH <inderbir.h@gmail.com>
2020-02-06 17:02:38 -0800

    feat(1091): Store template versions and reuse data, add metrics to template details page - temporary

--- app/templates/detail/route.js
+++ app/templates/detail/route.js
@@ -5,28 +5,52 @@ import { compareVersions } from 'screwdriver-ui/helpers/compare-versions';
 
 export default Route.extend({
   template: service(),
+  resultCache: Object(),
+  determineVersion(pVersion, verPayload, tagPayload) {
+    let version;
+
+    if (pVersion) {
+      const versionExists = verPayload.filter(t => t.version.startsWith(pVersion));
+      const tagExists = tagPayload.filter(t => t.tag === pVersion);
+
+      if (tagExists.length === 0 && versionExists.length === 0) {
+        this.transitionTo('/404');
+      }
+
+      if (versionExists.length > 0) {
+        // Sort commands by descending order
+        versionExists.sort((a, b) => compareVersions(b.version, a.version));
+        ({ version } = versionExists[0]);
+      }
+    }
+
+    return version || pVersion;
+  },
   model(params) {
+    this._super(...arguments);
+
+    if (this.resultCache.templateData) {
+      this.resultCache.versionOrTagFromUrl = this.determineVersion(
+        params.version,
+        this.resultCache.templateData,
+        this.resultCache.templateTagData
+      );
+
+      let newResult = {};
+
+      newResult.templateData = this.resultCache.templateData;
+      newResult.templateTagData = this.resultCache.templateTagData;
+      newResult.versionOrTagFromUrl = this.resultCache.versionOrTagFromUrl;
+
+      return newResult;
+    }
+
     return RSVP.all([
       this.template.getOneTemplateWithMetrics(`${params.namespace}/${params.name}`),
       this.template.getTemplateTags(params.namespace, params.name)
     ]).then(arr => {
       let [verPayload, tagPayload] = arr;
-      let version;
-
-      if (params.version) {
-        const versionExists = verPayload.filter(t => t.version.startsWith(params.version));
-        const tagExists = tagPayload.filter(t => t.tag === params.version);
-
-        if (tagExists.length === 0 && versionExists.length === 0) {
-          this.transitionTo('/404');
-        }
-
-        if (versionExists.length > 0) {
-          // Sort commands by descending order
-          versionExists.sort((a, b) => compareVersions(b.version, a.version));
-          ({ version } = versionExists[0]);
-        }
-      }
+      let version = this.determineVersion(params.version, verPayload, tagPayload);
 
       verPayload = verPayload.filter(t => t.namespace === params.namespace);
 
@@ -41,8 +65,10 @@ export default Route.extend({
       let result = {};
 
       result.templateData = verPayload;
-      result.versionOrTagFromUrl = version || params.version;
+      result.versionOrTagFromUrl = version;
       result.templateTagData = tagPayload;
+
+      this.resultCache = result;
 
       return result;
     });
