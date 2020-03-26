d22f04c446023749b8c54b3430e548e847ca3d18
yoshwata <yoshwata@yahoo-corp.jp>
2020-03-26 17:41:05 +0900

    fix(1383): Keep select the last viewed collection

--- app/components/collection-view/component.js
+++ app/components/collection-view/component.js
@@ -155,6 +155,10 @@ export default Component.extend({
 
       const getSha = sha => sha.substring(0, 7);
 
+      let viewingId = this.get('collection.id');
+
+      this.session.set('lastViewedCollectionId', viewingId);
+
       if (this.get('collection.pipelines')) {
         return this.get('collection.pipelines').map(pipeline => {
           const scm = scmService.getScm(pipeline.scmContext);
