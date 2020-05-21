a7262c40e8b4b4ba059566d6abb54e8e62aed0ab
Alan Dong <alandong@stanford.edu>
2020-05-21 11:45:01 -0700

    fix: integration test for pipeline-list-coverage-cell

--- tests/integration/components/pipeline-list-coverage-cell/component-test.js
+++ tests/integration/components/pipeline-list-coverage-cell/component-test.js
@@ -1,23 +1,30 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import RSVP from 'rsvp';
 
 module('Integration | Component | pipeline-list-coverage-cell', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     assert.expect(2);
-    this.set('value', {
-      buildId: 243421,
-      jobId: 21,
-      startTime: '2020-05-06T23:36:46.779Z',
-      endTime: '2020-05-06T23:50:18.590Z'
-    });
+    this.set(
+      'result',
+      RSVP.resolve({
+        envVars: {
+          SD_SONAR_AUTH_URL: 'https://api.screwdriver.cd/v4/coverage/token',
+          SD_SONAR_HOST: 'https://sonar.screwdriver.cd'
+        },
+        coverage: '71.4',
+        tests: 'N/A',
+        projectUrl: 'https://sonar.screwdriver.cd/dashboard?id=job%3A21'
+      })
+    );
 
-    await render(hbs`{{pipeline-list-coverage-cell value=value}}`);
+    await render(hbs`{{pipeline-list-coverage-cell result=result}}`);
 
-    assert.equal(this.element.textContent.trim(), '71.4');
     assert.dom('.coverage-value').exists({ count: 1 });
+    assert.equal(find('.coverage-value').textContent.trim(), '71.4');
   });
 });
