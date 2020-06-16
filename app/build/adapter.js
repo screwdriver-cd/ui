4eab6fdd7201e0e018b0c758f04cb6d81747b189
Jithin Emmanuel <jithine@verizonmedia.com>
2020-06-16 16:41:25 -0700

    enable overriding model key

--- app/build/adapter.js
+++ app/build/adapter.js
@@ -3,6 +3,7 @@ import BaseAdapter from 'screwdriver-ui/application/adapter';
 
 export default BaseAdapter.extend({
   queryRecord(store, type, data) {
+    this.modelKey = 'build';
     let url = `${ENV.APP.SDAPI_HOSTNAME}/${ENV.APP.SDAPI_NAMESPACE}/jobs/${data.jobId}/latestBuild`;
     let query = { status: data.status };
 
