8abf6acc9264a15fe6cfbf795a5dc6854e5d58f6
Kenta Kawaharada <kkawahar@yahoo-corp.jp>
2020-08-03 10:39:01 +0900

    fix: bringing back some of the unnecessary changes

--- app/app.js
+++ app/app.js
@@ -4,16 +4,16 @@ import { run } from '@ember/runloop';
 import Resolver from 'ember-resolver';
 import config from './config/environment';
 
-export default class App extends Application {
-  modulePrefix = config.modulePrefix;
-
-  podModulePrefix = config.podModulePrefix;
-
-  Resolver = Resolver;
-}
+const App = Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
 
 loadInitializers(App, config.modulePrefix);
 
 if (config.environment === 'development') {
   run.backburner.DEBUG = true;
 }
+
+export default App;
