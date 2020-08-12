00cb5d26af8e84c05289711a3ba6de8c07c8ebfb
Alan Dong <alandong2016@gmail.com>
2020-08-12 14:13:31 -0700

    fix: add eventId to track has scrollToHighlightedEvent or not
    - remove console.log

--- app/pipeline/events/show/route.js
+++ app/pipeline/events/show/route.js
@@ -12,7 +12,6 @@ export default class PipelineEventsShowRoute extends Route {
    * @return
    */
   scrollToHighlightedEvent() {
-    console.log('scrollToHighlightedEvent');
     const scrollConfig = { behavior: 'smooth', block: 'center' };
     const hightedEvent = document.querySelector('.highlighted');
 
@@ -24,14 +23,12 @@ export default class PipelineEventsShowRoute extends Route {
 
   async setupController(controller, model) {
     super.setupController(controller, model);
-    console.log('setupController');
 
     const { event_id: eventId } = this.paramsFor(this.routeName);
     const pipelineEventsController = this.controllerFor('pipeline.events');
     const desiredEvent = pipelineEventsController.events.findBy('id', eventId);
 
     if (!desiredEvent) {
-      // load desiredEvent
       await this.store.findRecord('event', eventId);
     }
 
@@ -43,21 +40,10 @@ export default class PipelineEventsShowRoute extends Route {
     }
   }
 
-  // @action
-  // activate() {
-  //   console.log('activate');
-  //   later(() => {
-  //     console.log('scheduleOnce');
-  //     scheduleOnce('afterRender', this, this.scrollToHighlightedEvent);
-  //   }, 50000);
-  // }
-
   @action
   didTransition() {
-    console.log(this.routeName, 'didTransition');
     if (!this.hasScrolled) {
       later(() => {
-        console.log('scheduleOnce');
         scheduleOnce('afterRender', this, this.scrollToHighlightedEvent);
       }, 5000);
     }
