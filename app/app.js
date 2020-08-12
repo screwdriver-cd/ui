b5955ded04fea5aa5e61b9d632895e8c1ceec7c3
Alan Dong <alandong2016@gmail.com>
2020-08-12 14:08:47 -0700

    fix: add console log

--- app/app.js
+++ app/app.js
@@ -7,7 +7,14 @@ import config from './config/environment';
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+  LOG_TRANSITIONS: true,
+
+  // Extremely detailed logging, highlighting every internal
+  // step made while transitioning into a route, including
+  // `beforeModel`, `model`, and `afterModel` hooks, and
+  // information about redirects and aborted transitions
+  LOG_TRANSITIONS_INTERNAL: true
 });
 
 loadInitializers(App, config.modulePrefix);
--- app/pipeline/events/show/route.js
+++ app/pipeline/events/show/route.js
@@ -1,9 +1,30 @@
 import Route from '@ember/routing/route';
-import { later } from '@ember/runloop';
+import { later, scheduleOnce } from '@ember/runloop';
+import { action } from '@ember/object';
 
 export default class PipelineEventsShowRoute extends Route {
+  eventId = undefined;
+
+  hasScrolled = false;
+
+  /**
+   * scroll to highlighted event
+   * @return
+   */
+  scrollToHighlightedEvent() {
+    console.log('scrollToHighlightedEvent');
+    const scrollConfig = { behavior: 'smooth', block: 'center' };
+    const hightedEvent = document.querySelector('.highlighted');
+
+    if (hightedEvent) {
+      hightedEvent.scrollIntoView(scrollConfig);
+      this.hasScrolled = true;
+    }
+  }
+
   async setupController(controller, model) {
     super.setupController(controller, model);
+    console.log('setupController');
 
     const { event_id: eventId } = this.paramsFor(this.routeName);
     const pipelineEventsController = this.controllerFor('pipeline.events');
@@ -14,13 +35,33 @@ export default class PipelineEventsShowRoute extends Route {
       await this.store.findRecord('event', eventId);
     }
 
-    later(() => {
-      // scroll to highlighted event
-      const scrollConfig = { behavior: 'smooth', block: 'center' };
-
-      document.querySelector('.highlighted').scrollIntoView(scrollConfig);
-    }, 5000);
-
     pipelineEventsController.set('selected', eventId);
+
+    if (eventId !== this.eventId) {
+      this.eventId = eventId;
+      this.hasScrolled = false;
+    }
+  }
+
+  // @action
+  // activate() {
+  //   console.log('activate');
+  //   later(() => {
+  //     console.log('scheduleOnce');
+  //     scheduleOnce('afterRender', this, this.scrollToHighlightedEvent);
+  //   }, 50000);
+  // }
+
+  @action
+  didTransition() {
+    console.log(this.routeName, 'didTransition');
+    if (!this.hasScrolled) {
+      later(() => {
+        console.log('scheduleOnce');
+        scheduleOnce('afterRender', this, this.scrollToHighlightedEvent);
+      }, 5000);
+    }
+
+    return true;
   }
 }
