19b2b6488b0c71f6369f2fc7ebda36bf72927b36
Alan Dong <alandong2016@gmail.com>
2020-09-02 10:29:20 -0700

    fix: assignment typo

--- app/components/pipeline-workflow/component.js
+++ app/components/pipeline-workflow/component.js
@@ -64,7 +64,7 @@ export default Component.extend({
           pValue.removeObject(currentEventParameterValue);
           pValue.unshift(currentEventParameterValue);
         } else {
-          buildParameters[pName].value = currentEventParameterValue;
+          buildParameters[pName] = currentEventParameterValue;
         }
       });
     }
