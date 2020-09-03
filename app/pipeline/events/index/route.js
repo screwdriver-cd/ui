e11655f98ac901948503da1e9988085d9dcc0196
Alan Dong <alandong2016@gmail.com>
2020-09-02 21:20:27 -0700

    fix: add redirect page for events

---
+++ app/pipeline/events/index/route.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { getWithDefault } from '@ember/object';
+
+export default class PipelineEventsIndexRoute extends Route {
+  redirect(model /* , transition */) {
+    const eventId = getWithDefault(model, 'events.firstObject.id', 0);
+    const { pipeline_id: pipelineId } = this.paramsFor('pipeline');
+
+    if (eventId) {
+      this.transitionTo('pipeline.events.show', pipelineId, eventId);
+    }
+  }
+}
