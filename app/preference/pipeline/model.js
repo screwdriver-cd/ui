c9d8ccdd624fad0b5398226656c83dc576909dca
Alan Dong <alandong2016@gmail.com>
2020-09-04 12:29:43 -0700

    fix: jobNameLength is not specific to each pipeline

--- app/preference/pipeline/model.js
+++ app/preference/pipeline/model.js
@@ -1,7 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class PreferencePipelineModel extends Model {
-  @attr('number') pipelineId;
+  @attr('string') pipelineId;
 
   @attr('number') jobNameLength;
 }
