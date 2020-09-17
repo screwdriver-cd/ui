69b44d3f219fbb54b5bac7cc24922cf4406516f3
Alan Dong <alandong2016@gmail.com>
2020-09-17 13:49:07 -0700

    fix: prevent clicking on pr event will trigger redirect

--- tests/integration/components/pipeline-events-list/component-test.js
+++ tests/integration/components/pipeline-events-list/component-test.js
@@ -1,8 +1,9 @@
 import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 module('Integration | Component | pipeline events list', function(hooks) {
   setupRenderingTest(hooks);
@@ -94,5 +95,125 @@ module('Integration | Component | pipeline events list', function(hooks) {
                       updateEvents=updateEventsMock}}`);
 
     assert.dom('.view').exists({ count: 2 });
+  });
+
+  test('it will redirect to event page when click on pipeline event', async function(assert) {
+    const events = [
+      EmberObject.create({
+        pipelineId: 10000,
+        id: 4,
+        startFrom: '~commit',
+        type: 'pipeline',
+        causeMessage: 'test',
+        commit: {
+          url: '#',
+          message: 'this was a test'
+        },
+        creator: {
+          url: '#',
+          name: 'batman'
+        },
+        createTimeWords: 'now',
+        durationText: '1 sec',
+        truncatedMessage: 'this was a test',
+        truncatedSha: 'abc124',
+        workflowGraph: {
+          nodes: [
+            { name: '~pr' },
+            { name: '~commit' },
+            { id: 1, name: 'main' },
+            { id: 2, name: 'A' },
+            { id: 3, name: 'B' }
+          ],
+          edges: [
+            { src: '~pr', dest: 'main' },
+            { src: '~commit', dest: 'main' },
+            { src: 'main', dest: 'A' },
+            { src: 'A', dest: 'B' }
+          ]
+        },
+        builds: [
+          { jobId: 1, id: 4, status: 'SUCCESS' },
+          { jobId: 2, id: 5, status: 'SUCCESS' },
+          { jobId: 3, id: 6, status: 'FAILURE' }
+        ]
+      })
+    ];
+
+    this.set('eventsMock', events);
+    this.set('updateEventsMock', page => {
+      assert.equal(page, 2);
+    });
+
+    await render(hbs`{{pipeline-events-list
+                      events=eventsMock
+                      eventsPage=1
+                      updateEvents=updateEventsMock}}`);
+
+    const route = this.owner.lookup('route:application');
+    const transitionToStub = sinon.stub(route, 'transitionTo');
+
+    await click('div.view');
+    assert.ok(transitionToStub.calledOnce, 'transitionTo was called once');
+  });
+
+  test('it will not redirect to event page when click on PR event', async function(assert) {
+    const events = [
+      EmberObject.create({
+        pipelineId: 10000,
+        id: 4,
+        startFrom: '~pr',
+        type: 'pr',
+        causeMessage: 'test',
+        commit: {
+          url: '#',
+          message: 'this was a test'
+        },
+        creator: {
+          url: '#',
+          name: 'batman'
+        },
+        createTimeWords: 'now',
+        durationText: '1 sec',
+        truncatedMessage: 'this was a test',
+        truncatedSha: 'abc124',
+        workflowGraph: {
+          nodes: [
+            { name: '~pr' },
+            { name: '~commit' },
+            { id: 1, name: 'main' },
+            { id: 2, name: 'A' },
+            { id: 3, name: 'B' }
+          ],
+          edges: [
+            { src: '~pr', dest: 'main' },
+            { src: '~commit', dest: 'main' },
+            { src: 'main', dest: 'A' },
+            { src: 'A', dest: 'B' }
+          ]
+        },
+        builds: [
+          { jobId: 1, id: 4, status: 'SUCCESS' },
+          { jobId: 2, id: 5, status: 'SUCCESS' },
+          { jobId: 3, id: 6, status: 'FAILURE' }
+        ]
+      })
+    ];
+
+    this.set('eventsMock', events);
+    this.set('updateEventsMock', page => {
+      assert.equal(page, 2);
+    });
+
+    await render(hbs`{{pipeline-events-list
+                      events=eventsMock
+                      eventsPage=1
+                      updateEvents=updateEventsMock}}`);
+
+    const route = this.owner.lookup('route:application');
+    const transitionToStub = sinon.stub(route, 'transitionTo');
+
+    await click('div.view');
+    assert.ok(transitionToStub.notCalled, 'transitionTo was not called');
   });
 });
