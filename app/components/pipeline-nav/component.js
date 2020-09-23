f7769fa74191d2bfd3bc3d817826dcbb7ada770b
Ibuki <ibu1224@users.noreply.github.com>
2020-09-24 06:18:52 +0900

    feat(2007): Add description to the pipeline page (#599)

--- app/components/pipeline-nav/component.js
+++ app/components/pipeline-nav/component.js
@@ -1,5 +1,23 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
-  classNames: ['row']
+  classNames: ['row'],
+  pipelineDescription: computed('pipeline', {
+    get() {
+      let description;
+
+      if (this.pipeline.annotations) {
+        if (this.pipeline.annotations['screwdriver.cd/pipelineDescription']) {
+          description = this.pipeline.annotations['screwdriver.cd/pipelineDescription'].replace(
+            /\n/g,
+            '<br>'
+          );
+        }
+      }
+
+      return htmlSafe(description);
+    }
+  })
 });
