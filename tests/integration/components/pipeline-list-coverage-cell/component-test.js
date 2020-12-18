0be50153d4f748c15ed062cffa3fef6dc2dfff4f
Alan Dong <adong>
2020-12-18 11:48:06 -0800

    fix: test case

--- tests/integration/components/pipeline-list-coverage-cell/component-test.js
+++ tests/integration/components/pipeline-list-coverage-cell/component-test.js
@@ -1,17 +1,30 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, settled } from '@ember/test-helpers';
+import Pretender from 'pretender';
+import ENV from 'screwdriver-ui/config/environment';
 import hbs from 'htmlbars-inline-precompile';
-import RSVP from 'rsvp';
 
 module('Integration | Component | pipeline-list-coverage-cell', function(hooks) {
   setupRenderingTest(hooks);
 
+  test('it renders with loading', async function(assert) {
+    assert.expect(2);
+    await render(hbs`{{pipeline-list-coverage-cell loading=true loaded=true}}`);
+
+    assert.dom('.coverage-value').exists({ count: 0 });
+    assert.equal(find('.coverage').textContent.trim(), 'Loading');
+  });
+
   test('it renders', async function(assert) {
     assert.expect(2);
-    this.set(
-      'result',
-      RSVP.resolve({
+
+    const server = new Pretender();
+
+    server.get(`${ENV.APP.SDAPI_HOSTNAME}/v4/coverage/info`, () => [
+      200,
+      { 'Content-Type': 'application/json' },
+      JSON.stringify({
         envVars: {
           SD_SONAR_AUTH_URL: 'https://api.screwdriver.cd/v4/coverage/token',
           SD_SONAR_HOST: 'https://sonar.screwdriver.cd'
@@ -20,11 +33,13 @@ module('Integration | Component | pipeline-list-coverage-cell', function(hooks) 
         tests: 'N/A',
         projectUrl: 'https://sonar.screwdriver.cd/dashboard?id=job%3A21'
       })
-    );
+    ]);
 
-    await render(hbs`{{pipeline-list-coverage-cell result=result}}`);
+    await render(hbs`{{pipeline-list-coverage-cell coverage=coverage loading=false loaded=true}}`);
 
-    assert.dom('.coverage-value').exists({ count: 1 });
-    assert.equal(find('.coverage-value').textContent.trim(), '71.4%');
+    return settled().then(() => {
+      assert.dom('.coverage-value').exists({ count: 1 });
+      assert.equal(find('.coverage-value').textContent.trim(), '71.4%');
+    });
   });
 });
