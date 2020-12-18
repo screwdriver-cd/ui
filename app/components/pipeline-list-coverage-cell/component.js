0817ac548214959ccd66947f5d2aed82e9f71295
Alan Dong <adong>
2020-12-18 10:50:58 -0800

    fix: lazy load coverage when row/cell is in view-port

--- app/components/pipeline-list-coverage-cell/component.js
+++ app/components/pipeline-list-coverage-cell/component.js
@@ -1,24 +1,38 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import InViewportMixin from 'ember-in-viewport';
 
-export default Component.extend({
+export default Component.extend(InViewportMixin, {
   shuttle: service(),
 
-  result: computed('value', {
-    async get() {
-      if (this.value.buildId) {
-        if (this.value.coverage) {
-          return this.value.coverage;
-        }
+  init() {
+    this._super(...arguments);
+
+    this.setProperties({
+      coverage: undefined,
+      loaded: false,
+      loading: false
+    });
+  },
+
+  async didEnterViewport() {
+    if (this.loaded === false) {
+      try {
+        this.set('loading', true);
         const coverage = await this.shuttle.fetchCoverage(this.value);
 
-        this.set('value', { ...this.value, coverage });
-
-        return coverage;
+        this.setProperties({
+          coverage,
+          loaded: true
+        });
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error('err', err);
+      } finally {
+        if (!this.isDestroyed && !this.isDestroying) {
+          this.set('loading', false);
+        }
       }
-
-      return '';
     }
-  })
+  }
 });
