5424cca6c4eef982682a76c8ffdddb506383925f
Alan Dong <alan>
2021-02-09 20:50:19 -0800

    fix: hide options to disable PR jobs

---
+++ app/helpers/starts-with.js
@@ -0,0 +1,13 @@
+import { helper } from '@ember/component/helper';
+
+/**
+ * return whether string starts with given string
+ * @param  {[String]}  str        Any string
+ * @param  {[String]}  searchString  Any string
+ * @return {Boolean}
+ */
+export function startsWith([str = '', searchString] /* , hash */) {
+  return str.startsWith(searchString);
+}
+
+export default helper(startsWith);
