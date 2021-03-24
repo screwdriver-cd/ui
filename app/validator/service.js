726c76b38c0722ce2558f07f518f259c6259b5a2
Tiffany Kyi <tiffanykyi@gmail.com>
2021-03-24 08:14:14 -0700

    fix: edit regex

--- app/validator/service.js
+++ app/validator/service.js
@@ -12,7 +12,7 @@ export default Service.extend({
    * @return {Boolean}
    */
   isTemplate(yaml) {
-    return /^name|\nname: |\nnamespace: /.test(yaml);
+    return /^name|\n+name: |\n+namespace: /.test(yaml);
   },
 
   /**
