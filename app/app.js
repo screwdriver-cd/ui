39ad48b14ae98e9e45787cfea4fc8b74be6f4094
Alan <adong@users.noreply.github.com>
2021-03-29 20:10:36 -0700

    adjust the height of the steps-list and the console is right.

--- app/app.js
+++ app/app.js
@@ -12,6 +12,24 @@ const App = Application.extend({
 
 loadInitializers(App, config.modulePrefix);
 
+// Make sure that the height of the steps-list and the console is right.
+function fixPartialViewHeight() {
+    const minHeight = 300;
+    const paddingBottom = 10;
+    for (const element of document.getElementsByClassName('partial-view')) {
+        element.style.height = '1px';
+        element.style.minHeight = '1px';
+        const rect = element.getBoundingClientRect();
+        const height = window.innerHeight - (rect.top + rect.height) - paddingBottom;
+        element.style.height = height + 'px';
+        element.style.minHeight = minHeight + 'px';
+    }
+}
+
+fixPartialViewHeight();
+window.addEventListener('resize', fixPartialViewHeight);
+window.setInterval(fixPartialViewHeight, 100);
+
 if (config.environment === 'development') {
   run.backburner.DEBUG = true;
 }
