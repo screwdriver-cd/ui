d6465597d72c9ad0a02ffa30d82d38e254b10ad0
Alan Dong <adong>
2021-04-06 11:40:27 -0700

    fix: hide avatar for GHE

--- app/components/user-link/component.js
+++ app/components/user-link/component.js
@@ -1,5 +1,11 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import ENV from 'screwdriver-ui/config/environment';
 
 export default Component.extend({
-  classNameBindings: ['large']
+  classNameBindings: ['large'],
+
+  isOpenSourceInstance: computed(function() {
+    return ENV.SDAPI_HOSTNAME === 'https://api.screwdriver.cd';
+  })
 });
