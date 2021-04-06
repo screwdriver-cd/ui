5b09f6c7b4c348baead857e6c7b0e1ab79d935a0
Alan Dong <adong>
2021-04-06 12:22:16 -0700

    fix: refactor show avatar to a feature flag

--- app/components/user-link/component.js
+++ app/components/user-link/component.js
@@ -5,7 +5,7 @@ import ENV from 'screwdriver-ui/config/environment';
 export default Component.extend({
   classNameBindings: ['large'],
 
-  isOpenSourceInstance: computed(function() {
-    return ENV.SDAPI_HOSTNAME === 'https://api.screwdriver.cd';
+  showAvatar: computed(function() {
+    return ENV.APP.SHOW_AVATAR === true;
   })
 });
