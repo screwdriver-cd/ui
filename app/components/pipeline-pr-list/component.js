7227414180f1f07c055650b09fd26202efe09910
Alan Dong <alan>
2021-10-20 11:20:47 -0700

    feat: make regular non-chained PR display workflowGraph
    - workflowGraph for prEvents will not display nodes starts with ~commmit

--- app/components/pipeline-pr-list/component.js
+++ app/components/pipeline-pr-list/component.js
@@ -1,8 +1,10 @@
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { computed, set } from '@ember/object';
 import { isActiveBuild } from 'screwdriver-ui/utils/build';
-
 export default Component.extend({
+  store: service(),
+
   didInsertElement() {
     this._super(...arguments);
     this.set('inited', false);
@@ -22,5 +24,22 @@ export default Component.extend({
         return isActiveBuild(status, endTime);
       });
     }
-  })
+  }),
+  actions: {
+    async selectPR() {
+      const pipelineId = this.get('pipeline.id');
+      const jobName = this.get('jobs.firstObject.name');
+      // extract prNumber from jobName
+      const prNum = jobName.split(/PR-(\d+):/)[1];
+      const events = await this.store.query('event', {
+        pipelineId,
+        page: 1,
+        count: 1,
+        prNum
+      });
+      const selectedEventId = events.get('firstObject.id');
+
+      set(this, 'selected', selectedEventId);
+    }
+  }
 });
--- app/pipeline/pulls/route.js
+++ app/pipeline/pulls/route.js
@@ -24,46 +24,44 @@ export default EventsRoute.extend({
 
     let events = [];
 
-    // fetch latest events which belongs to each PR jobs, if the prChain feature is enabled
-    if (this.get('pipeline.prChain')) {
-      // extracts prNumers, the name of PR jobs starts with `PR-$prNum:`
-      const prNumbers = jobsPromise.then(jobs =>
-        jobs
-          .filter(job => job.get('isPR'))
-          .map(job => job.get('name'))
-          .map(jobName => parseInt(jobName.slice('PR-'.length), 10))
-          .reduce((prNums, prNum) => {
-            if (prNums.includes(prNum)) {
-              return prNums;
-            }
+    // fetch latest events which belongs to each PR jobs
+    // extracts prNumers, the name of PR jobs starts with `PR-$prNum:`
+    const prNumbers = jobsPromise.then(jobs =>
+      jobs
+        .filter(job => job.get('isPR'))
+        .map(job => job.get('name'))
+        .map(jobName => parseInt(jobName.slice('PR-'.length), 10))
+        .reduce((prNums, prNum) => {
+          if (prNums.includes(prNum)) {
+            return prNums;
+          }
 
-            return prNums.concat(prNum);
-          }, [])
-      );
+          return prNums.concat(prNum);
+        }, [])
+    );
 
-      // iterate to fetch latest PR event which belongs to each PRs
-      events = prNumbers.then(prNums =>
-        Promise.all(
-          prNums.map(prNum =>
-            this.store.query('event', {
-              pipelineId: this.get('pipeline.id'),
-              page: 1,
-              count: 1,
-              prNum
-            })
-          )
-        ).then(queryReults => {
-          // merge PR events from separate query results
-          const prEvents = newArray();
+    // iterate to fetch latest PR event which belongs to each PRs
+    events = prNumbers.then(prNums =>
+      Promise.all(
+        prNums.map(prNum =>
+          this.store.query('event', {
+            pipelineId: this.get('pipeline.id'),
+            page: 1,
+            count: 1,
+            prNum
+          })
+        )
+      ).then(queryReults => {
+        // merge PR events from separate query results
+        const prEvents = newArray();
 
-          queryReults.forEach(prEvent => {
-            prEvents.pushObjects(prEvent.toArray());
-          });
+        queryReults.forEach(prEvent => {
+          prEvents.pushObjects(prEvent.toArray());
+        });
 
-          return prEvents;
-        })
-      );
-    }
+        return prEvents;
+      })
+    );
 
     return RSVP.hash({
       jobs: jobsPromise,
