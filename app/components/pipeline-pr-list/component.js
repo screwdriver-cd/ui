8ee67bedbaf52d373cf35cb4552d6c615e556f83
Alan Dong <alan>
2021-10-20 12:21:43 -0700

    fix: add highlight on selected PR

--- app/components/pipeline-pr-list/component.js
+++ app/components/pipeline-pr-list/component.js
@@ -1,15 +1,39 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import { computed, set } from '@ember/object';
+import { computed, set, get } from '@ember/object';
 import { isActiveBuild } from 'screwdriver-ui/utils/build';
 export default Component.extend({
   store: service(),
-
+  classNameBindings: ['highlighted'],
+  highlighted: computed('selectedEvent', 'eventId', {
+    get() {
+      return get(this, 'selectedEvent') === get(this, 'eventId');
+    }
+  }),
   didInsertElement() {
     this._super(...arguments);
     this.set('inited', false);
   },
   inited: true,
+
+  async init() {
+    this._super(...arguments);
+
+    const pipelineId = this.get('pipeline.id');
+    const jobName = this.get('jobs.firstObject.name');
+    // extract prNumber from jobName
+    const prNum = jobName.split(/PR-(\d+):/)[1];
+    const events = await this.store.query('event', {
+      pipelineId,
+      page: 1,
+      count: 1,
+      prNum
+    });
+    const currentEventId = events.get('firstObject.id');
+
+    set(this, 'eventId', currentEventId);
+  },
+
   showJobs: computed('jobs.@each.builds', 'inited', {
     get() {
       return this.inited || this.jobs.some(j => !!j.get('builds.length'));
@@ -26,20 +50,8 @@ export default Component.extend({
     }
   }),
   actions: {
-    async selectPR() {
-      const pipelineId = this.get('pipeline.id');
-      const jobName = this.get('jobs.firstObject.name');
-      // extract prNumber from jobName
-      const prNum = jobName.split(/PR-(\d+):/)[1];
-      const events = await this.store.query('event', {
-        pipelineId,
-        page: 1,
-        count: 1,
-        prNum
-      });
-      const selectedEventId = events.get('firstObject.id');
-
-      set(this, 'selected', selectedEventId);
+    selectPR() {
+      set(this, 'selected', this.get('eventId'));
     }
   }
 });
