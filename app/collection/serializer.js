e23fe0bd01c2d21297332344cc2f398167bf0f62
Alan Dong <adong>
2022-06-16 11:09:19 -0700

    fix: dry code and fixed failedBuildCount not present in pipeline
    * updated the serializer for collection model by modifying the response
    to insert links for metrics

--- app/collection/serializer.js
+++ app/collection/serializer.js
@@ -1,13 +1,43 @@
 import { assign } from '@ember/polyfills';
 import DS from 'ember-data';
+import { EmbeddedRecordsMixin } from '@ember-data/serializer/rest';
 
-export default DS.RESTSerializer.extend({
+export default DS.RESTSerializer.extend(EmbeddedRecordsMixin, {
+  attrs: {
+    pipelines: { embedded: 'always' }
+  },
+
+  normalizeResponse(store, typeClass, payload, id, requestType) {
+    console.log('here, collection serializer normalizeResponse');
+    console.log(
+      'typeClass',
+      typeClass,
+      'requestType',
+      requestType,
+      'payload',
+      payload
+    );
+
+    if (requestType === 'findRecord') {
+      payload.collection.pipelines.forEach(p => {
+        p.links = {
+          metrics: `/v4/pipelines/${p.id}/metrics?count=20&page=1`
+        };
+      });
+
+      console.log('updated payload collection', payload.collection);
+    }
+
+    return this._super(store, typeClass, payload, id, requestType);
+  },
   /**
    * Override the serializeIntoHash method
    * See http://emberjs.com/api/data/classes/DS.RESTSerializer.html#method_serializeIntoHash
    * @method serializeIntoHash
    */
   serializeIntoHash(hash, typeClass, snapshot) {
+    console.log('here, collection serializer serializeIntoHash');
+
     const dirty = snapshot.changedAttributes();
 
     Object.keys(dirty).forEach(key => {
--- app/components/collection-table-row/component.js
+++ app/components/collection-table-row/component.js
@@ -68,17 +68,18 @@ export default Component.extend({
     }
   },
   async updateEventMetrics() {
-    const metrics = await this.store
-      .query('metric', {
-        pipelineId: this.pipeline.id,
-        page: 1,
-        count: 20
-      })
-      .catch(() => {
-        this.setProperties({
-          storeQueryError: true
-        });
+    // const metrics = await this.store
+    //   .query('metric', {
+    //     pipelineId: this.pipeline.id,
+    //     page: 1,
+    //     count: 20
+    //   })
+
+    const metrics = await this.pipeline.get('metrics').catch(() => {
+      this.setProperties({
+        storeQueryError: true
       });
+    });
 
     const result = formatMetrics(metrics);
     const { eventsInfo, lastEventInfo } = result;
