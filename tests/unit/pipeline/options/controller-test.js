41fb1824c7f36e16105327b35962b1bc81be9cee
y-oksaku <yuokesak@yahoo-corp.jp>
2022-08-05 02:24:29 +0000

    fix: tests

--- tests/unit/pipeline/options/controller-test.js
+++ tests/unit/pipeline/options/controller-test.js
@@ -16,38 +16,6 @@ module('Unit | Controller | pipeline/options', function (hooks) {
     server.shutdown();
   });
 
-  test('it handles updating job state', function (assert) {
-    server.put('http://localhost:8080/v4/jobs/1234', () => [
-      200,
-      {},
-      JSON.stringify({ id: 1234 })
-    ]);
-
-    let controller = this.owner.lookup('controller:pipeline/options');
-
-    run(() => {
-      controller.store.push({
-        data: {
-          id: '1234',
-          type: 'job',
-          attributes: {
-            status: 'DISABLED'
-          }
-        }
-      });
-
-      controller.send('setJobStatus', '1234', 'ENABLED', 'testing');
-    });
-
-    return settled().then(() => {
-      const [request] = server.handledRequests;
-      const payload = JSON.parse(request.requestBody);
-
-      assert.equal(payload.state, 'ENABLED');
-      assert.equal(payload.stateChangeMessage, 'testing');
-    });
-  });
-
   test('it handles deleting pipelines', function (assert) {
     assert.expect(2);
     server.delete('http://localhost:8080/v4/pipelines/abc1234', () => [
