5956b34499ccb4bfc1b223773c8d01193a5f87c6
Alan <adong@users.noreply.github.com>
2022-08-30 17:52:07 -0700

    fix: remove displayJobNameLength feature (#818)

--- app/preference/pipeline/model.js
+++ app/preference/pipeline/model.js
@@ -1,8 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class PreferencePipelineModel extends Model {
-  @attr('number', { defaultValue: 20 }) displayJobNameLength;
-
   @attr('boolean', { defaultValue: true }) showPRJobs;
 
   @attr('string', { defaultValue: 'LOCAL_TIMEZONE' }) timestampFormat;
