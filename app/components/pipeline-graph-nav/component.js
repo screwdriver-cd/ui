58ac43ceafd34a9b8d8da41f8bbc14a1ea3de386
banusha2 <bollampally.anusha@yahooinc.com>
2022-09-30 15:17:53 -0400

    fix function name

--- app/components/pipeline-graph-nav/component.js
+++ app/components/pipeline-graph-nav/component.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import { computed, get } from '@ember/object';
 import { statusIcon } from 'screwdriver-ui/utils/build';
 import { toCustomLocaleString } from 'screwdriver-ui/utils/time-range';
 
@@ -42,29 +42,32 @@ export default Component.extend({
       return statusIcon(this.get('selectedEventObj.status'));
     }
   }),
-  startDate: computed('selectedEventObj.createTime', async function get() {
-    let startDate = 'n/a';
+  startDate: computed(
+    'selectedEventObj.createTime',
+    async function getStartDate() {
+      let startDate = 'n/a';
 
-    let timestampPreference;
+      let timestampPreference;
 
-    let timestamp;
+      let timestamp;
 
-    if (this.get('selectedEventObj.createTime')) {
-      const userPreferences = await this.userSettings.getUserPreference();
+      if (this.get('selectedEventObj.createTime')) {
+        const userPreferences = await this.userSettings.getUserPreference();
 
-      timestampPreference = get(userPreferences, 'timestampFormat');
+        timestampPreference = get(userPreferences, 'timestampFormat');
+      }
+      if (timestampPreference === 'UTC') {
+        timestamp = `${toCustomLocaleString(
+          new Date(this.get('selectedEventObj.createTime')),
+          { timeZone: 'UTC' }
+        )}`;
+      } else {
+        timestamp = `${toCustomLocaleString(
+          new Date(this.get('selectedEventObj.createTime'))
+        )}`;
+      }
+
+      return timestamp || startDate;
     }
-    if (timestampPreference === 'UTC') {
-      timestamp = `${toCustomLocaleString(
-        new Date(this.get('selectedEventObj.createTime')),
-        { timeZone: 'UTC' }
-      )}`;
-    } else {
-      timestamp = `${toCustomLocaleString(
-        new Date(this.get('selectedEventObj.createTime'))
-      )}`;
-    }
-
-    return timestamp || startDate;
-  })
+  )
 });
