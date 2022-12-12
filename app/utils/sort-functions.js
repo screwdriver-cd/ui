a02ac0cdb11a52505bab5fbab96b6f77dccad98e
banusha2 <bollampally.anusha@yahooinc.com>
2022-12-12 13:38:16 -0500

    add: jsdoc

--- app/utils/sort-functions.js
+++ app/utils/sort-functions.js
@@ -1,6 +1,15 @@
 import { get } from '@ember/object';
 
+/**
+ * this function sorts on the basis of status
+ * @param  {Object}  pipeline object
+ * @param {Object}  pipeline object
+ * @return {Array} sorted pipeline arrray
+ */
 export const sortByLastRunStatus = (a, b) => {
+  /**
+   * This array has sorting priority order
+   */
   const priorities = [
     'success',
     'running',
@@ -19,6 +28,12 @@ export const sortByLastRunStatus = (a, b) => {
   return priorities.indexOf(aStatus) - priorities.indexOf(bStatus);
 };
 
+/**
+ * this function sorts on the basis of name
+ * @param  {Object}  pipeline object
+ * @param {Object}  pipeline object
+ * @return {Array} sorted pipeline arrray
+ */
 export const sortByName = (a, b) => {
   const aName = get(a, 'scmRepo.name');
   const bName = get(b, 'scmRepo.name');
@@ -33,6 +48,12 @@ export const sortByName = (a, b) => {
   return 0;
 };
 
+/**
+ * this function sorts on the basis of last run
+ * @param  {Object}  pipeline object
+ * @param {Object}  pipeline object
+ * @return {Array} sorted pipeline arrray
+ */
 export const sortByLastRun = (a, b) => {
   const aCreateTime = get(a, 'lastRunEvent.createTime');
   const bCreateTime = get(b, 'lastRunEvent.createTime');
@@ -40,6 +61,12 @@ export const sortByLastRun = (a, b) => {
   return new Date(aCreateTime) - new Date(bCreateTime);
 };
 
+/**
+ * this function sorts on the basis of history
+ * @param  {Object}  pipeline object
+ * @param {Object}  pipeline object
+ * @return {Array} sorted pipeline arrray
+ */
 export const sortByHistory = (a, b) => {
   const aFailedBuildCount = get(a, 'failedBuildCount');
   const bFailedBuildCount = get(b, 'failedBuildCount');
@@ -47,10 +74,22 @@ export const sortByHistory = (a, b) => {
   return aFailedBuildCount - bFailedBuildCount;
 };
 
+/**
+ * this function sorts on the basis of branch
+ * @param  {Object}  pipeline object
+ * @param {Object}  pipeline object
+ * @return {Array} sorted pipeline arrray
+ */
 export const sortByBranch = (a, b) => {
   return a.scmRepo.branch - b.scmRepo.branch;
 };
 
+/**
+ * this function sorts on the basis of duration
+ * @param  {Object}  pipeline object
+ * @param {Object}  pipeline object
+ * @return {Array} sorted pipeline arrray
+ */
 export const sortByDuration = (a, b) => {
   const aDuration = get(a, 'lastRunEvent.duration');
   const bDuration = get(b, 'lastRunEvent.duration');
