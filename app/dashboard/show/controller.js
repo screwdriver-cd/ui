e6cf074c77310f71f1ccf79618121dd8df7ca1ea
banusha2 <bollampally.anusha@yahooinc.com>
2023-01-11 14:48:49 -0500

    fix: remove/add pipeline logic

--- app/dashboard/show/controller.js
+++ app/dashboard/show/controller.js
@@ -7,50 +7,44 @@ export default Controller.extend({
   collection: alias('model.collection'),
   shuttle: service(),
   actions: {
-    removePipeline(pipelineId) {
+    async removePipeline(pipelineId) {
       const collectionId = this.get('collection.id');
+      try {
+        await this.shuttle.removePipeline(collectionId, pipelineId);
+        const collection = await this.store.findRecord('collection', collectionId);
 
-      return this.store
-        .findRecord('collection', collectionId)
-        .then(collection => {
-          const pipelineIds = getWithDefault(collection, 'pipelineIds', []);
-
-          set(
-            collection,
-            'pipelineIds',
-            pipelineIds.filter(id => id !== pipelineId)
-          );
-
-          this.shuttle.removePipeline(collectionId, pipelineId);
-        });
+        return collection.save();
+      }
+      catch (e) {
+        return e;
+      }
     },
-    removeMultiplePipelines(removedPipelineIds) {
+    async removeMultiplePipelines(removedPipelineIds) {
       const collectionId = this.get('collection.id');
 
-      return this.store
-        .findRecord('collection', collectionId)
-        .then(collection => {
-          const pipelineIds = getWithDefault(collection, 'pipelineIds', []);
+      try {
+        await this.shuttle.removeMultiplePipelines(collectionId, removedPipelineIds);
+        const collection = await this.store.findRecord('collection', collectionId);
 
-          set(
-            collection,
-            'pipelineIds',
-            pipelineIds.filter(id => !removedPipelineIds.includes(id))
-          );
-
-          this.shuttle.removeMultiplePipelines(collectionId, removedPipelineIds);
-        });
+        return collection.save();
+      }
+      catch (e) {
+        return e;
+      }
     },
     onDeleteCollection() {
       this.transitionToRoute('home');
     },
-    addMultipleToCollection(addedPipelineIds, collectionId) {
-      return this.store
-        .findRecord('collection', collectionId)
-        .then(collection => {
+    async addMultipleToCollection(addedPipelineIds, collectionId) {
+      try {
+        await this.shuttle.updateCollection(collectionId, addedPipelineIds);
+        const collection = await this.store.findRecord('collection', collectionId);
 
-          this.shuttle.updateCollection(collectionId, addedPipelineIds);
-        });
+        return collection.save();
+      }
+      catch (e) {
+        return e;
+      }
     }
   }
 });
