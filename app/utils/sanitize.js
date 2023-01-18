ddb4df9ba3ddb2c9015da074b4b81fa703a14777
banusha2 <bollampally.anusha@yahooinc.com>
2023-01-18 10:51:35 -0500

    fix: lint

--- app/utils/sanitize.js
+++ app/utils/sanitize.js
@@ -1,57 +1,82 @@
 export const DEFAULT_OPTIONS = {
-	allowAttributes: {
-			a: [''],
-			p: ['data-'],
-	},
-	blockElements: ['script']
-}
+  allowAttributes: {
+    a: [''],
+    p: ['data-']
+  },
+  blockElements: ['script']
+};
 
+/**
+ * @param {Object} allowed Attributes
+ * @param {Object} blocked attributes
+ * @param {Object} allowed elements
+ * @return {Oject} containing method to sanitize string
+ */
 export function Sanitizer({
-	allowAttributes = DEFAULT_OPTIONS.allowAttributes,
-	blockElements = DEFAULT_OPTIONS.blockElements,
-	allowElements
+  allowAttributes = DEFAULT_OPTIONS.allowAttributes,
+  blockElements = DEFAULT_OPTIONS.blockElements,
+  allowElements
 } = {}) {
-	const parser = new DOMParser();
+  const parser = new DOMParser();
 
-	function sanitiseChildren(node) {
-			for (let child of node.children) {
-					sanitiseNode(child);
-			}
-	}
-	function sanitiseNode(node) {
-		switch(node.nodeType) {
-				case 1:
-					const tagName = node.tagName.toLowerCase();
-					if(blockElements.includes(tagName) || (allowElements && !allowElements?.includes(tagName))) {
-						node.parentElement.removeChild(node);
-					} else {
-							const attrs = allowAttributes[tagName] || [];
-							const hasDataAttrsWild = attrs.includes('data-');
-							node.getAttributeNames().forEach(attr => {
-									if (node.getAttribute(attr).toLowerCase().indexOf('cript:') > 0) {
-										node.removeAttribute(attr);
-									} else if(hasDataAttrsWild && attr.startsWith('data-')) {
-										return;
-									}
-									if(!attrs.includes(attr)) {
-										node.removeAttribute(attr)
-									}
-								});
-								sanitiseChildren(node);
-							}
-				default:
-						
-					return;
-			}
-	}
-	
-	
-	return {
-		parseFromString(html) {
-			const fragment = parser.parseFromString(html, 'text/html').body;
-			sanitiseNode(fragment);
+  /**
+   * @param {node}
+   * @return {}
+   */
+  function sanitiseNode(node) {
+    switch (node.nodeType) {
+      case 1:
+        const tagName = node.tagName.toLowerCase();
 
-			return fragment.textContent;
-		}
-	}
+        if (
+          blockElements.includes(tagName) ||
+          (allowElements && !allowElements?.includes(tagName))
+        ) {
+          node.parentElement.removeChild(node);
+        } else {
+          const attrs = allowAttributes[tagName] || [];
+          const hasDataAttrsWild = attrs.includes('data-');
+
+          node.getAttributeNames().forEach(attr => {
+            if (node.getAttribute(attr).toLowerCase().indexOf('cript:') > 0) {
+              node.removeAttribute(attr);
+            } else if (hasDataAttrsWild && attr.startsWith('data-')) {
+              return;
+            }
+            if (!attrs.includes(attr)) {
+              node.removeAttribute(attr);
+            }
+          });
+          sanitiseChildren(node);
+        }
+        break;
+      default:
+    }
+  }
+
+  /**
+   *
+   * @param {Node} node
+   */
+
+  function sanitiseChildren(node) {
+    for (let child of node.children) {
+      sanitiseNode(child);
+    }
+  }
+
+  return {
+    /**
+     *
+     * @param {String} html string to be sanitize
+     * @returns {String} sanitized string
+     */
+    parseFromString(html) {
+      const fragment = parser.parseFromString(html, 'text/html').body;
+
+      sanitiseNode(fragment);
+
+      return fragment.textContent;
+    }
+  };
 }
