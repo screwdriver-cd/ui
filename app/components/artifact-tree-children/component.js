1dacd26efef05174ea0fe1921144b92e9933dd8f
Haruka Kawamura <itleigns@gmail.com>
2023-03-08 02:39:18 +0900

    fix(2840): fix artifacts deeplinks (#892)
    Co-authored-by: Keisuke Kumada <kekumada@yahoo-corp.jp>

---
+++ app/components/artifact-tree-children/component.js
@@ -0,0 +1,27 @@
+import { computed } from '@ember/object';
+import Component from '@ember/component';
+
+export default Component.extend({
+  nextDir: computed('selectedArtifactRelative', {
+    get() {
+      const dividePath = this.selectedArtifactRelative.split('/');
+
+      if (dividePath.length <= 1) {
+        return '';
+      }
+
+      return dividePath[0];
+    }
+  }),
+  selectedArtifactRelativeChild: computed('selectedArtifactRelative', {
+    get() {
+      const dividePath = this.selectedArtifactRelative.split('/');
+
+      if (dividePath.length <= 1) {
+        return '';
+      }
+
+      return this.selectedArtifactRelative.substring(dividePath[0].length + 1);
+    }
+  })
+});
