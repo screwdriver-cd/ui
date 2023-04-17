8402715a789db2641082732a3a4f91d31b2cabf0
Ibuki <ibu1224@users.noreply.github.com>
2023-04-17 23:00:37 +0900

    Update component.js

--- app/components/template-header/component.js
+++ app/components/template-header/component.js
@@ -36,7 +36,7 @@ export default Component.extend({
       });
     },
     updateTrust(fullName, toTrust) {
-      this.set('isTrusted', toTrust)
+      this.set('isTrusted', toTrust);
       this.onUpdateTrust(fullName, toTrust);
     }
   }
