867d2ddb84236058c0147b7865f04f24a871819b
itleigns <itleigns@gmail.com>
2023-05-23 16:16:38 +0900

    fix

--- app/components/artifact-tree-content/component.js
+++ app/components/artifact-tree-content/component.js
@@ -7,24 +7,33 @@ export default Component.extend({
 
   artifactPath: computed('node.directory', 'node.file.a_attr.href', {
     get() {
+      // Recursively search the child for files and identify paths
+      const getParentPath = node => {
+        let childPath;
+
+        if (node.children === undefined) {
+          childPath = node.a_attr.href.substring(
+            node.a_attr.href.split('artifacts/')[0].length + 10
+          );
+        } else {
+          childPath = getParentPath(node.children[0]);
+        }
+
+        return childPath.substring(
+          0,
+          childPath.length -
+            childPath.split('/')[childPath.split('/').length - 1].length -
+            1
+        );
+      };
+
       if (this.node.directory === undefined) {
         return this.node.file.a_attr.href.substring(
           this.node.file.a_attr.href.split('artifacts/')[0].length + 10
         );
       }
 
-      console.log('WSWSWS');
-      console.log(this.node.directory);
-      const childPath = this.node.directory[0].a_attr.href.substring(
-        this.node.directory[0].a_attr.href.split('artifacts/')[0].length + 10
-      );
-
-      return `${childPath.substring(
-        0,
-        childPath.length -
-          childPath.split('/')[childPath.split('/').length - 1].length -
-          1
-      )}/`;
+      return `${getParentPath(this.node.directory[0])}/`;
     }
   }),
 
