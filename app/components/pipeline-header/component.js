102c0904e13aae6db833e5cb74b8b96777770cf4
itleigns <itleigns@gmail.com>
2023-11-16 12:00:36 +0900

    fix branch move for root dir

--- app/components/pipeline-header/component.js
+++ app/components/pipeline-header/component.js
@@ -28,8 +28,9 @@ export default Component.extend({
   }),
   sameRepoPipeline: computed('pipeline', {
     get() {
+      const repositoryUri = this.pipeline.scmUri.substring(0,this.pipeline.scmUri.indexOf(':', this.pipeline.scmUri.indexOf(':') + 1))
       return this.pipelineService.getSiblingPipeline(this.pipeline.scmRepo.name).then(value =>
-        value.toArray().filter(pipe => pipe.id !== this.pipeline.id && this.pipeline.scmUri.substring(0, this.pipeline.scmUri.lastIndexOf(':')) === pipe.scmUri.substring(0, pipe.scmUri.lastIndexOf(':'))).map((pipe, i) => ({
+        value.toArray().filter(pipe => pipe.id !== this.pipeline.id && repositoryUri === pipe.scmUri.substring(0,pipe.scmUri.indexOf(':', pipe.scmUri.indexOf(':') + 1))).map((pipe, i) => ({
           index: i,
           url: `/pipelines/${pipe.id}`,
           branchAndRootDir: pipe.scmRepo.rootDir ? `${pipe.scmRepo.branch}:${pipe.scmRepo.rootDir}` : pipe.scmRepo.branch
