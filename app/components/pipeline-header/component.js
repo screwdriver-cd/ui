33969cb9d7e407758f7e2eeb501e54b03e043ee6
Alan Dong <adong@users.noreply.github.com>
2023-12-05 12:01:17 -0800

    fix: remove unused code from rebased from master

--- app/components/pipeline-header/component.js
+++ app/components/pipeline-header/component.js
@@ -48,30 +48,19 @@ export default Component.extend({
       };
     }
   }),
-  sameRepoPipeline: computed('pipeline.scmRepo.name', 'pipeline.{id,scmUri}', {
+  sameRepoPipeline: computed('pipeline', {
     get() {
       const [scm, repositoryId] = this.pipeline.scmUri.split(':');
-
-      return this.pipelineService
-        .getSiblingPipeline(this.pipeline.scmRepo.name)
-        .then(value =>
-          value
-            .toArray()
-            .filter(pipe => {
-              const [s, r] = pipe.scmUri.split(':');
-
-              return (
-                pipe.id !== this.pipeline.id && scm === s && repositoryId === r
-              );
-            })
-            .map((pipe, i) => ({
-              index: i,
-              url: `/pipelines/${pipe.id}`,
-              branchAndRootDir: pipe.scmRepo.rootDir
-                ? `${pipe.scmRepo.branch}:${pipe.scmRepo.rootDir}`
-                : pipe.scmRepo.branch
-            }))
-        );
+      return this.pipelineService.getSiblingPipeline(this.pipeline.scmRepo.name).then(value =>
+        value.toArray().filter(pipe => {
+          const [s, r] = pipe.scmUri.split(':');
+          return pipe.id !== this.pipeline.id && scm === s && repositoryId === r;
+        }).map((pipe, i) => ({
+          index: i,
+          url: `/pipelines/${pipe.id}`,
+          branchAndRootDir: pipe.scmRepo.rootDir ? `${pipe.scmRepo.branch}:${pipe.scmRepo.rootDir}` : pipe.scmRepo.branch
+        }))
+      );
     }
   }),
   actions: {
