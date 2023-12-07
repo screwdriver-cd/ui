b127973e31bffe77988eb94b5a326b80808ef1be
klu909 <klu13960@gmail.com>
2023-12-07 15:43:05 -0800

    fix: sanitize user input before displaying info message

--- app/components/info-message/component.js
+++ app/components/info-message/component.js
@@ -1,7 +1,14 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { sanitizeString } from 'screwdriver-ui/helpers/sanitize-string';
 
 export default Component.extend({
   type: 'info',
+  sanitizedMessgae: computed('message', {
+    get() {
+      return sanitizeString(this.message);
+    }
+  }),
   actions: {
     clearMessage: function clearMessage() {
       this.set('message', null);
