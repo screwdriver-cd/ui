df8a14981fb1bbeee1ab320fe4fd2e4aefbb1e5a
Alan Dong <adong@users.noreply.github.com>
2024-01-02 09:35:07 -0800

    fix: add more tests

--- tests/integration/components/info-message/component-test.js
+++ tests/integration/components/info-message/component-test.js
@@ -37,4 +37,29 @@ module('Integration | Component | info message', function (hooks) {
 
     assert.dom('.alert > span').hasText('batman');
   });
+
+  test('it renders image tag with empty string', async function (assert) {
+    const userInput = `Image tag: <img src="#notanimage" onerror="alert('unsafe script injection from warning!')" />`;
+
+    this.userInput = userInput;
+
+    await render(hbs`<InfoMessage @message={{this.userInput}} />`);
+
+    assert.dom('.alert > span').hasText('Image tag:');
+  });
+
+  test('it renders link with href attribute only', async function (assert) {
+    const userInput = `message - blocked by some builds, <a href="/builds/913516" rel="noopener" onclick="alert('xss test');">913516</a>`;
+
+    this.userInput = userInput;
+
+    await render(hbs`<InfoMessage @message={{this.userInput}} />`);
+
+    assert
+      .dom('.alert > span')
+      .hasText('message - blocked by some builds, 913516');
+    assert.dom('.alert > span > a').hasAttribute('href', '/builds/913516');
+    assert.dom('.alert > span > a').hasNoAttribute('rel');
+    assert.dom('.alert > span > a').hasNoAttribute('onclck');
+  });
 });
