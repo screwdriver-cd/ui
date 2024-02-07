1032460ea231ddafd17eef24930fc43739bd5452
Ming Hay Luk <luk.minghay@gmail.com>
2024-02-06 16:26:58 -0800

    Keep selectedEventObj.status

--- app/components/workflow-graph-d3/component.js
+++ app/components/workflow-graph-d3/component.js
@@ -24,6 +24,7 @@ export default Component.extend({
     'jobs.@each.{isDisabled,state,stateChanger}',
     'minified',
     'prJobs',
+    'selectedEventObj.status',
     'selectedEventObj.prNum',
     'showDownstreamTriggers',
     'showPRJobs',
