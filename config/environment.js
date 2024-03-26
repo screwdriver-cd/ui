7dff5b51c37bf807984eea79ef2ecafc9b62de27
Alan Dong <alan>
2024-03-26 08:34:32 -0700

    add route test

--- config/environment.js
+++ config/environment.js
@@ -98,8 +98,8 @@ module.exports = environment => {
     }
 
     ENV.APP.EVENT_RELOAD_TIMER = 5000;
-    ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
---
+++ tests/unit/templates/catch-all/route-test.js
@@ -0,0 +1,34 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'screwdriver-ui/tests/helpers';
+import { visit, currentURL } from '@ember/test-helpers';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+
+module('Unit | Route | templates/catch-all', function (hooks) {
+  setupTest(hooks);
+
+  test('visiting /templates/namespace', async function (assert) {
+    await visit('/templates/job/namespace');
+
+    assert.equal(currentURL(), '/login');
+  });
+
+  test('visiting /templates/namespace/name', async function (assert) {
+    await visit('/templates/job/namespace/name');
+
+    assert.equal(currentURL(), '/login');
+  });
+
+  test('visiting authed /templates/namespace', async function (assert) {
+    await authenticateSession({ token: 'fakeToken' });
+    await visit('/templates/job/namespace');
+
+    assert.equal(currentURL(), '/templates/job/namespace');
+  });
+
+  test('visiting authed /templates/namespace', async function (assert) {
+    await authenticateSession({ token: 'fakeToken' });
+    await visit('/templates/job/namespace/name');
+
+    assert.equal(currentURL(), '/templates/job/namespace/name');
+  });
+});
