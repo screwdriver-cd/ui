cd1702c9efae209fcf933b8bf0fc579cf88aeff8
Alan Dong <alan>
2024-03-25 23:49:02 -0700

    fix: remove console.log

--- app/templates/job/detail/controller.js
+++ app/templates/job/detail/controller.js
@@ -72,8 +72,6 @@ export default Controller.extend({
           pipelineIds.map(p => this.store.findRecord('pipeline', p))
         );
 
-        console.log(pipelines);
-
         return pipelines;
       }
     }
