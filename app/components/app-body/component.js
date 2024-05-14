95c03648e29af7e0ea9aacdd75860da630d37fb0
Ming Hay Luk <luk.minghay@gmail.com>
2024-05-13 17:11:15 -0700

    Use pipeline-page class for new pipeline pages

--- app/components/app-body/component.js
+++ app/components/app-body/component.js
@@ -5,7 +5,10 @@ export default class AppBody extends Component {
   @service router;
 
   get class() {
-    if (this.router.currentRouteName.startsWith('pipeline')) {
+    if (
+      this.router.currentRouteName.startsWith('pipeline') ||
+      this.router.currentRouteName.startsWith('v2.pipeline')
+    ) {
       return 'pipeline-page';
     }
 
