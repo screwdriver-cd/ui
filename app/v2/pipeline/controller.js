ae1272e0ee11d5d47c5494c3e344599124d36dd1
Alan Dong <adong@users.noreply.github.com>
2024-05-14 12:59:17 -0700

    fix: code cleanup

--- app/v2/pipeline/controller.js
+++ app/v2/pipeline/controller.js
@@ -5,12 +5,24 @@ import { action } from '@ember/object';
 export default class NewPipelineController extends Controller {
   @service session;
 
+  @service router;
+
   get collections() {
     return this.model.collections;
   }
 
   get pipeline() {
     return this.model.pipeline;
+  }
+
+  get isEventsPullsJobsRoute() {
+    const { currentRouteName } = this.router;
+
+    return [
+      'v2.pipeline.events.show',
+      'v2.pipeline.pulls',
+      'v2.pipeline.jobs.index'
+    ].includes(currentRouteName);
   }
 
   @action
