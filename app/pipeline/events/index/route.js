c4a19c862e8dbcdc8cfa29652a7c52b8f40d4c03
Alan Dong <adong@users.noreply.github.com>
2024-05-22 12:12:20 -0700

    fix: revert changes

--- app/pipeline/events/index/route.js
+++ app/pipeline/events/index/route.js
@@ -1,11 +1,15 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { get } from '@ember/object';
 
 export default class PipelineEventsIndexRoute extends Route {
   @service router;
 
   redirect(model = {} /* , transition */) {
-    const eventId = model.events.firstObject.id || 0;
+    const eventId =
+      get(model, 'events.firstObject.id') === undefined
+        ? 0
+        : get(model, 'events.firstObject.id');
     const { pipeline_id: pipelineId } = this.paramsFor('pipeline');
 
     if (eventId) {
