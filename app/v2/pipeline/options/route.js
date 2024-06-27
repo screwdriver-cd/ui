4fe06fac674dd6ecf255afec7aaea0c806504f52
Alan Dong <adong@users.noreply.github.com>
2024-06-27 13:52:42 -0700

    feat: add options to v2 route

--- app/v2/pipeline/options/route.js
+++ app/v2/pipeline/options/route.js
@@ -1,3 +1,30 @@
-import Route from '@ember/routing/route';
+// import Route from '@ember/routing/route';
 
-export default class NewPipelineOptionsRoute extends Route {}
+// export default class NewPipelineOptionsRoute extends Route {}
+
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
+
+export default Route.extend({
+  session: service(),
+  router: service(),
+  routeAfterAuthentication: 'pipeline.options',
+  model() {
+    // Guests should not access this page
+    if (get(this, 'session.data.authenticated.isGuest')) {
+      this.router.transitionTo('pipeline');
+    }
+
+    const { pipeline } = this.modelFor('v2.pipeline');
+
+    // Prevent double render when jobs list updates asynchronously
+    return pipeline.get('jobs').then(jobs => ({ pipeline, jobs }));
+  },
+  actions: {
+    willTransition() {
+      // Reset error message when switching pages
+      this.controller.set('errorMessage', '');
+    }
+  }
+});
