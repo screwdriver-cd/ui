74e020e7117463915744e2b78d6c41d2eeb7800f
Ming Hay Luk <luk.minghay@gmail.com>
2024-07-01 18:05:32 -0700

    Refactor pipeline secrets to not use ember data

--- app/v2/pipeline/secrets/route.js
+++ app/v2/pipeline/secrets/route.js
@@ -1,42 +1,49 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import { get } from '@ember/object';
 
 export default class NewPipelineSecretsRoute extends Route {
-  @service session;
-
-  @service store;
-
   @service router;
 
-  model() {
+  @service session;
+
+  @service shuttle;
+
+  async beforeModel() {
+    // Guests should not access this page
+    if (this.session.data.authenticated.isGuest) {
+      this.router.transitionTo('v2.pipeline');
+    }
+  }
+
+  async model() {
     // Refresh error message
     this.controllerFor('v2.pipeline.secrets').set('errorMessage', '');
 
-    // Guests should not access this page
-    if (get(this, 'session.data.authenticated.isGuest')) {
-      this.router.transitionTo('pipeline');
-    }
-
     const { pipeline } = this.modelFor('v2.pipeline');
-    const secrets = pipeline.get('secrets');
+    const pipelineId = pipeline.id;
 
-    this.store.unloadAll('token');
-
-    return this.store
-      .findAll('token', { adapterOptions: { pipelineId: pipeline.get('id') } })
-      .then(tokens => ({
-        tokens,
-        secrets,
-        pipeline
-      }))
-      .catch(error => {
-        this.controllerFor('pipeline.secrets').set(
+    const secrets = await this.shuttle
+      .fetchFromApi('get', `/pipelines/${pipelineId}/secrets`)
+      .catch(err => {
+        this.controllerFor('v2.pipeline.secrets').set(
           'errorMessage',
-          error.errors[0].detail
+          err.message
         );
 
-        return { secrets, pipeline };
+        return [];
       });
+
+    const tokens = await this.shuttle
+      .fetchFromApi('get', `/pipelines/${pipelineId}/tokens`)
+      .catch(err => {
+        this.controllerFor('v2.pipeline.secrets').set(
+          'errorMessage',
+          err.message
+        );
+
+        return [];
+      });
+
+    return { pipeline, secrets, tokens };
   }
 }
