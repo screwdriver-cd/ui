8d04c6fb8065a6833fe61801277bb14efdf9a255
Ming-Hay <157658916+minghay@users.noreply.github.com>
2024-07-10 18:40:50 -0700

    feat: Modal for adding a pipeline to a collection (#1084)

---
+++ tests/unit/components/collection/modal/add-to-collection/util-test.js
@@ -0,0 +1,40 @@
+import { module, test } from 'qunit';
+import {
+  createCollectionBody,
+  getCollectionsWithoutPipeline
+} from 'screwdriver-ui/components/collection/modal/add-to-collection/util';
+
+module(
+  'Unit | Component | collection/modal/add-to-collection/util',
+  function () {
+    test('getCollectionsWithoutPipeline returns sorted list of collections', function (assert) {
+      assert.deepEqual(
+        getCollectionsWithoutPipeline(
+          [
+            { id: 1, name: 'collection', pipelineIds: [] },
+            { id: 2, name: 'abc', pipelineIds: [456] },
+            { id: 3, name: 'something', pipelineIds: [456, 987] },
+            { id: 4, name: 'xyz', pipelineIds: [456, 123, 987] },
+            { id: 5, name: 'zzz', pipelineIds: [456, 987] }
+          ],
+          123
+        ),
+        [
+          { id: 2, name: 'abc', pipelineIds: [456] },
+          { id: 1, name: 'collection', pipelineIds: [] },
+          { id: 3, name: 'something', pipelineIds: [456, 987] },
+          { id: 5, name: 'zzz', pipelineIds: [456, 987] }
+        ]
+      );
+    });
+
+    test('createCollectionBody creates request body', function (assert) {
+      assert.deepEqual(createCollectionBody('Test Collection', '', 123), {
+        name: 'Test Collection',
+        description: '',
+        pipelineIds: [123],
+        type: 'normal'
+      });
+    });
+  }
+);
