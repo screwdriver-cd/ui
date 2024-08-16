79e5f936009db6805be140f12db9de54b0aa4aa0
Ming Hay Luk <luk.minghay@gmail.com>
2024-08-16 09:05:15 -0700

    Fix route to properly redirect to latest event

--- app/v2/pipeline/events/index/route.js
+++ app/v2/pipeline/events/index/route.js
@@ -1,19 +1,13 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import { get } from '@ember/object';
 
 export default class NewPipelineEventsIndexRoute extends Route {
   @service router;
 
-  redirect(model = {} /* , transition */) {
-    const eventId =
-      get(model, 'events.firstObject.id') === undefined
-        ? 0
-        : get(model, 'events.firstObject.id');
-    const { pipeline_id: pipelineId } = this.paramsFor('v2.pipeline');
-
-    if (eventId) {
-      this.router.transitionTo('v2.pipeline.events.show', pipelineId, eventId);
-    }
+  redirect(model) {
+    this.router.replaceWith(
+      'v2.pipeline.events.show',
+      model.latestCommitEvent.id
+    );
   }
 }
