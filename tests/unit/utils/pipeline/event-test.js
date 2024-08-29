5c4ddec59c5e1ded5c0a78452779fd4cacc096e7
Ming-Hay <157658916+minghay@users.noreply.github.com>
2024-08-28 17:00:44 -0700

    feat: Add more event utils (#1127)
    Co-authored-by: Vonny Jap <jap.vonny@gmail.com>

--- tests/unit/utils/pipeline/event-test.js
+++ tests/unit/utils/pipeline/event-test.js
@@ -1,66 +1,71 @@
 import { module, test } from 'qunit';
-import { isSkipped } from 'screwdriver-ui/utils/pipeline/event';
+import {
+  getStatus,
+  isComplete,
+  isSkipped
+} from 'screwdriver-ui/utils/pipeline/event';
 
 module('Unit | Utility | Pipeline | event', function () {
-  test('pr events are not skipped', function (assert) {
-    assert.equal(isSkipped({ type: 'pr' }, []), false);
-  });
-
-  test('pipeline events with builds are not skipped', function (assert) {
+  test('isSkipped returns correct value', function (assert) {
+    assert.equal(isSkipped({ type: 'pr' }), false);
+    assert.equal(isSkipped({ type: 'pipeline' }), false);
     assert.equal(
-      isSkipped(
-        {
-          type: 'pipeline'
-        },
-        [{ id: 1 }]
-      ),
-      false
-    );
-  });
-
-  test('pipeline events without skip trigger in commit messages are not skipped', function (assert) {
-    assert.equal(
-      isSkipped(
-        {
-          type: 'pipeline',
-          commit: { message: 'Made some changes' }
-        },
-        []
-      ),
+      isSkipped({
+        type: 'pipeline'
+      }),
       false
     );
     assert.equal(
-      isSkipped(
-        {
-          type: 'pipeline',
-          commit: { message: 'skip ci Made some changes' }
-        },
-        []
-      ),
+      isSkipped({
+        type: 'pipeline',
+        commit: { message: 'Made some changes' }
+      }),
       false
     );
-  });
-
-  test('pipeline events with skip trigger in commit messages are skipped', function (assert) {
     assert.equal(
-      isSkipped(
-        {
-          type: 'pipeline',
-          commit: { message: '[skip ci] Made some changes' }
-        },
-        []
-      ),
+      isSkipped({
+        type: 'pipeline',
+        commit: { message: 'skip ci Made some changes' }
+      }),
+      false
+    );
+    assert.equal(
+      isSkipped({
+        type: 'pipeline',
+        commit: { message: '[skip ci] Made some changes' }
+      }),
       true
     );
     assert.equal(
-      isSkipped(
-        {
-          type: 'pipeline',
-          commit: { message: '[ci skip] Made some changes' }
-        },
-        []
-      ),
+      isSkipped({
+        type: 'pipeline',
+        commit: { message: '[ci skip] Made some changes' }
+      }),
       true
     );
+  });
+
+  test('isComplete returns correct value', function (assert) {
+    assert.equal(isComplete(null), true);
+    assert.equal(isComplete([]), true);
+    assert.equal(isComplete([{ status: 'UNSTABLE' }]), false);
+    assert.equal(
+      isComplete([{ status: 'UNSTABLE', endTime: '2024-04-01T00:00:00.000Z' }]),
+      true
+    );
+    assert.equal(isComplete([{ status: 'SUCCESS' }]), true);
+    assert.equal(isComplete([{ status: 'QUEUED' }]), false);
+  });
+
+  test('getStatus returns correct value', function (assert) {
+    assert.equal(
+      getStatus({ commit: { message: '[skip ci]' } }, []),
+      'SKIPPED'
+    );
+    assert.equal(getStatus({}, null), 'UNKNOWN');
+    assert.equal(getStatus({}, []), 'UNKNOWN');
+    assert.equal(getStatus({}, [{ status: 'SUCCESS' }]), 'SUCCESS');
+    assert.equal(getStatus({}, [{ status: 'FROZEN' }]), 'FROZEN');
+    assert.equal(getStatus({}, [{ status: 'CREATED' }]), 'RUNNING');
   });
 });
