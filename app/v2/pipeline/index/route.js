7a56408219dbd250b2bc24cbc69b7a81c773a3fa
Ming-Hay <157658916+minghay@users.noreply.github.com>
2024-08-28 17:13:34 -0700

    fix: Simplify and update route implementation (#1133)

--- app/v2/pipeline/index/route.js
+++ app/v2/pipeline/index/route.js
@@ -2,22 +2,15 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class NewPipelineIndexRoute extends Route {
-  @service
-  router;
+  @service router;
 
   beforeModel() {
     const { pipeline } = this.modelFor('v2.pipeline');
 
     if (pipeline.childPipelines) {
-      this.router.transitionTo('pipeline.child-pipelines');
+      this.router.replaceWith('pipeline.child-pipelines');
     } else {
-      this.router.transitionTo('v2.pipeline.events');
+      this.router.replaceWith('v2.pipeline.events');
     }
   }
-
-  /* eslint-disable camelcase */
-  model(/* { pipeline_id } */) {
-    return this;
-  }
-  /* eslint-enable camelcase */
 }
