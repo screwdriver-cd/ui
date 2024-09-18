6dd29a3efedbeb6ae35e61fb07da5202795c3979
Ming Hay Luk <luk.minghay@gmail.com>
2024-09-18 11:23:51 -0700

    Table cell for test coverage information

---
+++ app/components/pipeline/jobs/table/cell/coverage/component.js
@@ -0,0 +1,52 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+
+export default class PipelineJobsTableCellCoverageComponent extends Component {
+  @service shuttle;
+
+  @tracked latestBuild;
+
+  @tracked coverage;
+
+  constructor() {
+    super(...arguments);
+
+    const { job } = this.args.record;
+
+    this.args.record.onCreate(job, builds => {
+      if (builds.length > 0) {
+        this.latestBuild = builds[builds.length - 1];
+        const { pipeline } = this.args.record;
+
+        this.shuttle
+          .fetchFromApi('get', '/coverage/info', {
+            jobId: job.id,
+            buildId: this.latestBuild.id,
+            startTime: this.latestBuild.startTime,
+            endTime: this.latestBuild.endTime,
+            jobName: job.name,
+            pipelineName: pipeline.name,
+            projectKey: `pipeline:${pipeline.id}`
+          })
+          .then(response => {
+            if (response.coverage) {
+              this.coverage =
+                response.coverage === 'N/A'
+                  ? response.coverage
+                  : `${response.coverage}%`;
+            }
+          })
+          .catch(() => {
+            this.coverage = null;
+          });
+      }
+    });
+  }
+
+  willDestroy() {
+    super.willDestroy();
+
+    this.args.record.onDestroy(this.args.record.job);
+  }
+}
