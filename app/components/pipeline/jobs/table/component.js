98d5812352edfcafcfa94f44d4e15f4f45b02196
Ming-Hay <157658916+minghay@users.noreply.github.com>
2024-09-23 13:24:03 -0700

    feat: v2 jobs table glimmer component (#1171)
    Co-authored-by: Vonny Jap <jap.vonny@gmail.com>

---
+++ app/components/pipeline/jobs/table/component.js
@@ -0,0 +1,121 @@
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import DataReloader from './dataReloader';
+
+const INITIAL_PAGE_SIZE = 10;
+
+export default class PipelineJobsTableComponent extends Component {
+  @service shuttle;
+
+  @service('emt-themes/ember-bootstrap-v5') emberModelTableBootstrapTheme;
+
+  dataReloader;
+
+  data = [];
+
+  columns = [
+    {
+      title: 'JOB',
+      propertyName: 'jobName',
+      className: 'job-column',
+      component: 'jobCell'
+    },
+    {
+      title: 'HISTORY',
+      className: 'history-column',
+      component: 'historyCell'
+    },
+    {
+      title: 'DURATION',
+      className: 'duration-column',
+      component: 'durationCell'
+    },
+    {
+      title: 'START TIME',
+      className: 'start-time-column',
+      component: 'startTimeCell'
+    },
+    {
+      title: 'COVERAGE',
+      className: 'coverage-column',
+      component: 'coverageCell'
+    },
+    {
+      title: 'METRICS',
+      className: 'metrics-column',
+      component: 'metricsCell'
+    },
+    {
+      title: 'ACTIONS',
+      className: 'actions-column',
+      component: 'actionsCell'
+    }
+  ];
+
+  constructor() {
+    super(...arguments);
+
+    const jobIds = this.args.jobs.map(job => job.id);
+
+    this.dataReloader = new DataReloader(
+      this.shuttle,
+      jobIds,
+      INITIAL_PAGE_SIZE
+    );
+    const initialJobIds = this.dataReloader.newJobIds();
+
+    this.args.jobs.forEach(job => {
+      this.data.push({
+        job,
+        jobName: job.name,
+        pipeline: this.args.pipeline,
+        jobs: this.args.jobs,
+        timestampFormat: this.args.userSettings.timestampFormat,
+        onCreate: (jobToMonitor, buildsCallback) => {
+          this.dataReloader.addCallbackForJobId(
+            jobToMonitor.id,
+            buildsCallback
+          );
+        },
+        onDestroy: jobToMonitor => {
+          this.dataReloader.removeCallbacksForJobId(jobToMonitor.id);
+        }
+      });
+    });
+
+    this.dataReloader.fetchBuildsForJobs(initialJobIds).then(() => {
+      this.dataReloader.start();
+    });
+  }
+
+  willDestroy() {
+    super.willDestroy();
+
+    this.dataReloader.destroy();
+  }
+
+  get theme() {
+    const theme = this.emberModelTableBootstrapTheme;
+
+    theme.searchPlaceholderMsg = 'Search for job by name';
+    theme.table = 'table table-condensed table-hover table-sm';
+
+    return theme;
+  }
+
+  @action
+  onDisplayDataChanged(data) {
+    const jobIds = data.filteredContent.map(record => record.job.id);
+
+    this.dataReloader.updateJobsMatchingFilter(
+      jobIds,
+      data.pageSize,
+      data.currentPageNumber
+    );
+
+    this.dataReloader
+      .fetchBuildsForJobs(this.dataReloader.newJobIds())
+      .then(() => {});
+  }
+}
