384554204d8094db444d0308691e1372df65f778
Dayanand Sagar <sagar1312@gmail.com>
2024-10-30 13:09:15 -0700

    fix(3211): Find workflow graph node for a virtual build by job name (#1211)

--- tests/unit/job/model-test.js
+++ tests/unit/job/model-test.js
@@ -13,4 +13,20 @@ module('Unit | Model | job', function (hooks) {
 
     assert.ok(!!model);
   });
+
+  test('it returns pr parent job name', function (assert) {
+    const model = run(() =>
+      this.owner.lookup('service:store').createRecord('job', {
+        name: 'PR-123:deploy'
+      })
+    );
+
+    run(() => {
+      // job is a PR job
+      assert.equal(model.get('prParentJobName'), 'deploy');
+      // job is not a PR job
+      model.set('name', 'publish');
+      assert.equal(model.get('prParentJobName'), null);
+    });
+  });
 });
