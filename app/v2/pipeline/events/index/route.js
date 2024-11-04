50ed4ecebf51e7819ff567038d95a0e7ce8d34dd
Ming-Hay <157658916+minghay@users.noreply.github.com>
2024-11-04 15:49:30 -0800

    feat: Refactor event routes to use pipeline component (#1223)

--- app/v2/pipeline/events/index/route.js
+++ app/v2/pipeline/events/index/route.js
@@ -1,13 +1,42 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { NotFoundError } from 'ember-ajax/errors';
 
 export default class NewPipelineEventsIndexRoute extends Route {
-  @service router;
+  @service shuttle;
 
-  redirect(model) {
-    this.router.replaceWith(
-      'v2.pipeline.events.show',
-      model.latestCommitEvent.id
-    );
+  async model() {
+    const model = this.modelFor('v2.pipeline.events');
+    const pipelineId = model.pipeline.id;
+
+    const latestCommitEvent = await this.shuttle
+      .fetchFromApi('get', `/pipelines/${pipelineId}/latestCommitEvent`)
+      .then(event => {
+        return event;
+      })
+      .catch(err => {
+        if (err instanceof NotFoundError) {
+          return null;
+        }
+
+        return undefined;
+      });
+
+    return { ...model, latestCommitEvent };
+  }
+
+  afterModel(model) {
+    const { latestCommitEvent } = model;
+
+    if (latestCommitEvent) {
+      const transition = this.replaceWith(
+        'v2.pipeline.events.show',
+        latestCommitEvent.id
+      );
+
+      transition.data = {
+        latestCommitEvent
+      };
+    }
   }
 }
--- app/v2/pipeline/events/route.js
+++ app/v2/pipeline/events/route.js
@@ -6,22 +6,15 @@ export default class NewPipelineEventsRoute extends Route {
 
   async model() {
     const model = this.modelFor('v2.pipeline');
-    const pipelineId = this.paramsFor('v2.pipeline').pipeline_id;
 
     const userSettings = await this.shuttle.fetchFromApi(
       'get',
       '/users/settings'
     );
 
-    const latestCommitEvent = await this.shuttle.fetchFromApi(
-      'get',
-      `/pipelines/${pipelineId}/latestCommitEvent`
-    );
-
     return {
       ...model,
-      userSettings,
-      latestCommitEvent
+      userSettings
     };
   }
 }
