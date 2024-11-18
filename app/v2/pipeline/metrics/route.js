84cd1e751f9ba16c5652e1cfa222d01dd88a4f8c
VonnyJap <jap.vonny@gmail.com>
2024-11-17 17:11:06 -0800

    add redirect to v1 metrics page

--- app/v2/pipeline/metrics/route.js
+++ app/v2/pipeline/metrics/route.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class NewPipelineMetricsRoute extends Route {}
+export default class NewPipelineMetricsRoute extends Route {
+  beforeModel(transition) {
+    const id = transition.to.parent.params.pipeline_id;
+
+    // redirect to the v1 pipeline metrics page
+    this.transitionTo('pipeline.metrics', id);
+  }
+}
