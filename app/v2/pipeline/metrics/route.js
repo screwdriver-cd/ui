8974568f255741dbe3453fd888059dee96234ede
Vonny Jap <jap.vonny@gmail.com>
2024-11-18 10:57:09 -0800

    Update app/v2/pipeline/metrics/route.js
    Co-authored-by: Ming-Hay <157658916+minghay@users.noreply.github.com>

--- app/v2/pipeline/metrics/route.js
+++ app/v2/pipeline/metrics/route.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class NewPipelineMetricsRoute extends Route {
-  @service router; // Ensure router service is injected
 
   beforeModel(transition) {
     this.replaceWith('pipeline.metrics', transition.to.parent.params.pipeline_id);
