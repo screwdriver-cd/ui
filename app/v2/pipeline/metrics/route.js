aad2751f99a84a0fd7688d76226cfb6187c9fa2a
VonnyJap <jap.vonny@gmail.com>
2024-11-17 23:11:42 -0800

    add unit tests

--- app/v2/pipeline/metrics/route.js
+++ app/v2/pipeline/metrics/route.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class NewPipelineMetricsRoute extends Route {
+  @service router; // Ensure router service is injected
+
   beforeModel(transition) {
     const id = transition.to.parent.params.pipeline_id;
+    // Use RouterService to handle redirection
 
-    // redirect to the v1 pipeline metrics page
-    this.transitionTo('pipeline.metrics', id);
+    this.router.transitionTo('pipeline.metrics', id);
   }
 }
--- tests/unit/v2/pipeline/metrics/route-test.js
+++ tests/unit/v2/pipeline/metrics/route-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'screwdriver-ui/tests/helpers';
+import sinon from 'sinon';
 
 module('Unit | Route | v2/pipeline/metrics', function (hooks) {
   setupTest(hooks);
@@ -8,5 +9,31 @@ module('Unit | Route | v2/pipeline/metrics', function (hooks) {
     let route = this.owner.lookup('route:v2/pipeline/metrics');
 
     assert.ok(route);
+  });
+
+  test('it redirects to pipeline.metrics with the correct id', function (assert) {
+    let route = this.owner.lookup('route:v2/pipeline/metrics');
+
+    let routerService = this.owner.lookup('service:router');
+
+    let transitionStub = sinon.stub(routerService, 'transitionTo');
+
+    let transition = {
+      to: {
+        parent: {
+          params: {
+            pipeline_id: '123'
+          }
+        }
+      }
+    };
+
+    route.beforeModel(transition);
+    assert.ok(transitionStub.calledOnce, 'transitionTo was called once');
+    assert.ok(
+      transitionStub.calledWith('pipeline.metrics', '123'),
+      'transitionTo was called with the correct arguments'
+    );
+    transitionStub.restore();
   });
 });
