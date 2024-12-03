90f1debb978114c0a7fd9eafee52373ffe4946fc
Tiffany K <tiffanykyi@gmail.com>
2024-12-02 17:09:34 -0800

    fix: Use dropdown instead of prefix filter

--- app/components/pipeline/modal/search-event/component.js
+++ app/components/pipeline/modal/search-event/component.js
@@ -16,44 +16,40 @@ export default class PipelineModalSearchEventComponent extends Component {
 
   isPr;
 
+  searchField;
+
   constructor() {
     super(...arguments);
 
     this.searchResults = [];
     this.invalidSha = false;
+    this.searchField = null;
 
     this.isPr = this.router.currentRouteName.includes('pulls');
   }
 
   @action
+  setSelection(selected) {
+    this.searchField = selected;
+  }
+
+  @action
   handleInput(inputEvent) {
     const inputValue = inputEvent.target.value.trim();
-    const [searchField, ...searchParts] = inputValue.split(':');
-
-    let searchValue = searchParts.join(':').trim(); // In case the search value has a colon
 
     // Validate searchValue and determine urlFilter
     const validHex = /^[0-9a-f]{1,40}$/;
-    const validFields = ['sha', 'author', 'creator', 'message'];
 
-    let urlFilter = 'message'; // Default filter
+    let urlFilter = this.searchField || 'message'; // Default filter
 
-    if (searchField === 'sha' && searchValue) {
-      this.invalidSha = !validHex.test(searchValue);
-      if (!this.invalidSha) {
-        urlFilter = 'sha';
-      }
-    } else if (validFields.includes(searchField)) {
-      urlFilter = searchField;
-    } else {
-      // Default case, use message as filter
-      searchValue = inputValue;
+    if (this.searchField === 'sha' && inputValue) {
+      this.invalidSha = !validHex.test(inputValue);
     }
 
     // Construct search URL with proper query parameters
     const baseUrl = `/pipelines/${
       this.args.pipeline.id
-    }/events?${urlFilter}=${encodeURIComponent(searchValue)}`;
+    }/events?${urlFilter}=${encodeURIComponent(inputValue)}`;
     const url = `${baseUrl}&type=${this.isPr ? 'pr' : 'pipeline'}`;
 
     this.shuttle.fetchFromApi('get', url).then(events => {
