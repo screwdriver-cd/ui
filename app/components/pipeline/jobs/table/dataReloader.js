31c19e3314d92a66bd78f733f05c0ad118a3a558
Vonny Jap <jap.vonny@gmail.com>
2024-12-12 13:58:22 -0800

    feat(3200): incorporating selection to render number of historical builds in jobs list view  (#1291)
    Co-authored-by: Ming-Hay <157658916+minghay@users.noreply.github.com>

--- app/components/pipeline/jobs/table/dataReloader.js
+++ app/components/pipeline/jobs/table/dataReloader.js
@@ -97,8 +97,12 @@ export default class DataReloader {
       });
   }
 
-  setNumBuilds(numBuilds) {
+  async setNumBuilds(numBuilds) {
+    if (this.numBuilds === numBuilds) {
+      return;
+    }
     this.numBuilds = numBuilds;
+    await this.fetchBuildsForJobs(this.jobIdsMatchingFilter);
   }
 
   start() {
