e3344fb23d01772fa445090b836cc648a9548617
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-02-07 13:06:40 -0800

    Extract isParameterized to a util (#1330)

--- app/components/pipeline/modal/confirm-action/util.js
+++ app/components/pipeline/modal/confirm-action/util.js
@@ -19,3 +19,19 @@ export function truncateMessage(commitMessage) {
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+/**
+ * Determine if the action has parameters
+ * @param pipeline
+ * @param event
+ * @returns {boolean}
+ */
+export function isParameterized(pipeline, event) {
+  const pipelineParameters = pipeline.parameters || {};
+  const eventParameters = event.meta?.parameters || {};
+
+  return (
+    Object.keys(pipelineParameters).length > 0 ||
+    Object.keys(eventParameters).length > 0
+  );
+}
