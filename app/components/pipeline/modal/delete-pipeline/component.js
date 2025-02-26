d3fb94eabb559a9a362c75e9cbcee0ef050ae533
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-02-26 15:18:13 -0800

    feat: Modal for deleting pipeline (#1350)

---
+++ app/components/pipeline/modal/delete-pipeline/component.js
@@ -0,0 +1,47 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+export default class PipelineModalDeletePipelineComponent extends Component {
+  @service router;
+
+  @service shuttle;
+
+  @tracked errorMessage = null;
+
+  @tracked isAwaitingResponse = false;
+
+  @tracked wasActionSuccessful = false;
+
+  @tracked confirmDeleteInput;
+
+  get isDeleteButtonDisabled() {
+    if (this.wasActionSuccessful || this.isAwaitingResponse) {
+      return true;
+    }
+
+    return (
+      !this.confirmDeleteInput ||
+      this.confirmDeleteInput !== this.args.pipeline.name
+    );
+  }
+
+  @action
+  async deletePipeline() {
+    this.isAwaitingResponse = true;
+
+    await this.shuttle
+      .fetchFromApi('delete', `/pipelines/${this.args.pipelineId}`)
+      .then(() => {
+        this.args.closeModal(true);
+      })
+      .catch(err => {
+        this.wasActionSuccessful = false;
+        this.errorMessage = err.message;
+      })
+      .finally(() => {
+        this.isAwaitingResponse = false;
+      });
+  }
+}
