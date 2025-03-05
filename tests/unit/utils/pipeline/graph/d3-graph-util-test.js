a02fe18908b9c4f8b2250c92c13a84dfe1ea2b71
Yuki Okesaku <yuokesak@yahoo-corp.jp>
2025-03-05 11:40:50 +0900

    fix: tests

--- tests/unit/utils/pipeline/graph/d3-graph-util-test.js
+++ tests/unit/utils/pipeline/graph/d3-graph-util-test.js
@@ -55,7 +55,7 @@ module('Unit | Utility | pipeline-graph | d3-graph-util', function () {
         { name: 'job1' },
         { name: 'job2', displayName: '0123456789' },
         { name: 'job3' },
-        { name: '~sd@1:foo', displayName: '0123456789/0123456789' }
+        { name: '~sd@1:foo', remotename: '0123456789/0123456789' }
       ]
     };
 
