0ae65ea59c25110d728a8bcec80a420c72474c64
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-03-05 12:46:47 -0800

    feat: Create modal for starting all child pipelines (#1354)

---
+++ app/components/pipeline/modal/start-children/component.js
@@ -0,0 +1,42 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+export default class PipelineModalStartChildrenComponent extends Component {
+  @service shuttle;
+
+  @service pipelinePageState;
+
+  @tracked errorMessage = null;
+
+  @tracked isAwaitingResponse = false;
+
+  @tracked wasActionSuccessful = false;
+
+  get isStartAllButtonDisabled() {
+    return this.wasActionSuccessful || this.isAwaitingResponse;
+  }
+
+  @action
+  async startChildren() {
+    this.isAwaitingResponse = true;
+
+    await this.shuttle
+      .fetchFromApi(
+        'post',
+        `/pipelines/${this.pipelinePageState.getPipelineId()}/startall`
+      )
+      .then(() => {
+        this.args.closeModal(true);
+        this.wasActionSuccessful = true;
+      })
+      .catch(err => {
+        this.wasActionSuccessful = false;
+        this.errorMessage = err.message;
+      })
+      .finally(() => {
+        this.isAwaitingResponse = false;
+      });
+  }
+}
