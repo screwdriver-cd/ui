28bb5462914a379aa0085073eab3d62eb3c4dd9d
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-03-06 14:07:28 -0800

    feat: Child pipeline route (#1362)

--- app/v2/pipeline/index/route.js
+++ app/v2/pipeline/index/route.js
@@ -4,13 +4,7 @@ import { service } from '@ember/service';
 export default class NewPipelineIndexRoute extends Route {
   @service router;
 
-  @service pipelinePageState;
-
   beforeModel() {
-    if (this.pipelinePageState.getPipeline().childPipelines) {
-      this.router.replaceWith('pipeline.child-pipelines');
-    } else {
-      this.router.replaceWith('v2.pipeline.events');
-    }
+    this.router.replaceWith('v2.pipeline.events');
   }
 }
--- tests/integration/components/pipeline/nav/component-test.js
+++ tests/integration/components/pipeline/nav/component-test.js
@@ -2,13 +2,30 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'screwdriver-ui/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
 
 module('Integration | Component | pipeline/nav', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
+    const pipelinePageState = this.owner.lookup('service:pipeline-page-state');
+
+    sinon.stub(pipelinePageState, 'getPipeline').returns({});
+
     await render(hbs`<Pipeline::Nav />`);
 
     assert.dom('#pipeline-nav a').exists({ count: 4 });
+  });
+
+  test('it renders link to child pipelines', async function (assert) {
+    const pipelinePageState = this.owner.lookup('service:pipeline-page-state');
+
+    sinon
+      .stub(pipelinePageState, 'getPipeline')
+      .returns({ childPipelines: [{ id: 1 }] });
+
+    await render(hbs`<Pipeline::Nav />`);
+
+    assert.dom('#pipeline-nav a').exists({ count: 5 });
   });
 });
