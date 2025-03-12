ce7bcda7f9253dada59cb4f98b65763eedcddc6e
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-03-12 14:14:29 -0700

    fix: Drop router injection (#1367)

--- app/v2/pipeline/index/route.js
+++ app/v2/pipeline/index/route.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route';
-import { service } from '@ember/service';
 
 export default class NewPipelineIndexRoute extends Route {
-  @service router;
-
   beforeModel() {
-    this.router.replaceWith('v2.pipeline.events');
+    this.replaceWith('v2.pipeline.events');
   }
 }
