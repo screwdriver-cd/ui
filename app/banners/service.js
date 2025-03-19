87abe313c91db2cebd1f8644d1e0d5ee5bb532e1
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-03-19 16:52:16 -0700

    feat: Creates an injectable service for banners (#1373)

---
+++ app/banners/service.js
@@ -0,0 +1,57 @@
+import Service, { service } from '@ember/service';
+
+export default class BannersService extends Service {
+  @service shuttle;
+
+  globalBanners;
+
+  pipelineBanners;
+
+  displayingBanners;
+
+  bannerCallback;
+
+  constructor() {
+    super(...arguments);
+
+    this.pipelineBanners = new Map();
+  }
+
+  registerCallback(callback) {
+    this.bannerCallback = callback;
+  }
+
+  async getGlobalBanners() {
+    if (!this.globalBanners) {
+      await this.shuttle
+        .fetchFromApi('get', '/banners?isActive=true&scope=GLOBAL')
+        .then(banners => {
+          this.globalBanners = banners;
+          this.displayingBanners = banners;
+        });
+    }
+    if (!this.displayingBanners) {
+      this.displayingBanners = this.globalBanners;
+    }
+
+    this.bannerCallback(this.displayingBanners);
+  }
+
+  async getPipelineBanners(pipelineId) {
+    const pipelineBanners = this.pipelineBanners.has(pipelineId)
+      ? this.pipelineBanners.get(pipelineId)
+      : await this.shuttle
+          .fetchFromApi(
+            'get',
+            `/banners?isActive=true&scope=PIPELINE&scopeId=${pipelineId}`
+          )
+          .then(banners => {
+            this.pipelineBanners.set(pipelineId, banners);
+
+            return banners;
+          });
+
+    this.displayingBanners = pipelineBanners.concat(this.globalBanners);
+    this.bannerCallback(this.displayingBanners);
+  }
+}
