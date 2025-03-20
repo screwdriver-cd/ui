52d4ab346fb240450df5f509f7614dcd4f4344b9
Ming Hay Luk <luk.minghay@gmail.com>
2025-03-20 12:04:25 -0700

    Update unit tests

--- tests/unit/templates/catch-all/route-test.js
+++ tests/unit/templates/catch-all/route-test.js
@@ -1,5 +1,3 @@
-import Service from '@ember/service';
-import { Promise as EmberPromise } from 'rsvp';
 import { module, test } from 'qunit';
 import { setupTest } from 'screwdriver-ui/tests/helpers';
 import { visit, currentURL } from '@ember/test-helpers';
@@ -8,54 +6,25 @@ import { authenticateSession } from 'ember-simple-auth/test-support';
 module('Unit | Route | templates/catch-all', function (hooks) {
   setupTest(hooks);
 
+  hooks.beforeEach(function () {
+    const banners = this.owner.lookup('service:banners');
+
+    banners.globalBanners = [];
+  });
+
   test('visiting /templates/namespace', async function (assert) {
-    assert.expect(3);
-
-    const shuttleService = Service.extend({
-      fetchBanners: (scope, scopeId) => {
-        assert.equal(scope, 'GLOBAL');
-        assert.equal(scopeId, undefined);
-
-        return new EmberPromise(resolve => resolve([]));
-      }
-    });
-
-    this.owner.register('service:shuttle', shuttleService);
-
     await visit('/templates/job/namespace');
 
     assert.equal(currentURL(), '/login');
   });
 
   test('visiting /templates/namespace/name', async function (assert) {
-    const shuttleService = Service.extend({
-      fetchBanners: (scope, scopeId) => {
-        assert.equal(scope, 'GLOBAL');
-        assert.equal(scopeId, undefined);
-
-        return new EmberPromise(resolve => resolve([]));
-      }
-    });
-
-    this.owner.register('service:shuttle', shuttleService);
-
     await visit('/templates/job/namespace/name');
 
     assert.equal(currentURL(), '/login');
   });
 
   test('visiting authed /templates/namespace', async function (assert) {
-    const shuttleService = Service.extend({
-      fetchBanners: (scope, scopeId) => {
-        assert.equal(scope, 'GLOBAL');
-        assert.equal(scopeId, undefined);
-
-        return new EmberPromise(resolve => resolve([]));
-      }
-    });
-
-    this.owner.register('service:shuttle', shuttleService);
-
     await authenticateSession({ token: 'fakeToken' });
     await visit('/templates/job/namespace');
 
@@ -63,17 +32,6 @@ module('Unit | Route | templates/catch-all', function (hooks) {
   });
 
   test('visiting authed /templates/namespace', async function (assert) {
-    const shuttleService = Service.extend({
-      fetchBanners: (scope, scopeId) => {
-        assert.equal(scope, 'GLOBAL');
-        assert.equal(scopeId, undefined);
-
-        return new EmberPromise(resolve => resolve([]));
-      }
-    });
-
-    this.owner.register('service:shuttle', shuttleService);
-
     await authenticateSession({ token: 'fakeToken' });
     await visit('/templates/job/namespace/name');
 
