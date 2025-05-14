a17c809cfa5a3fc919c835d609862a59c88aa3d9
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-05-14 14:40:50 -0700

    fix: combine edit and override secret modals (#1416)

--- tests/integration/components/pipeline/secrets-and-tokens/secrets/modal/edit/component-test.js
+++ tests/integration/components/pipeline/secrets-and-tokens/secrets/modal/edit/component-test.js
@@ -11,14 +11,25 @@ module(
 
     let shuttle;
 
+    let secret;
+
     hooks.beforeEach(function () {
+      const pipelinePageState = this.owner.lookup('service:pipelinePageState');
+
       shuttle = this.owner.lookup('service:shuttle');
 
+      sinon.stub(pipelinePageState, 'getPipelineId').returns(1);
+
+      secret = {
+        id: 123,
+        name: 'TEST',
+        allowInPr: false,
+        inherited: false,
+        overridden: false
+      };
+
       this.setProperties({
-        secret: {
-          name: 'TEST',
-          allowInPr: false
-        },
+        secret,
         closeModal: () => {}
       });
     });
@@ -31,10 +42,56 @@ module(
         />`
       );
 
-      assert.dom('.modal-header').hasText('Edit secret TEST ×');
+      assert.dom('.modal-header').exists();
       assert.dom('#error-message').doesNotExist();
-      assert.dom('#submit-secret').exists({ count: 1 });
+      assert.dom('#submit-secret').exists();
       assert.dom('#submit-secret').isDisabled();
+    });
+
+    test('it renders correct header text', async function (assert) {
+      await render(
+        hbs`<Pipeline::SecretsAndTokens::Secrets::Modal::Edit
+            @secret={{this.secret}}
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('.modal-header').hasText('Edit secret TEST ×');
+    });
+
+    test('it renders correct header for overridden secrets', async function (assert) {
+      secret.inherited = true;
+      secret.overridden = true;
+
+      this.setProperties({
+        secret
+      });
+
+      await render(
+        hbs`<Pipeline::SecretsAndTokens::Secrets::Modal::Edit
+            @secret={{this.secret}}
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('.modal-header').hasText('Edit secret TEST ×');
+    });
+
+    test('it renders correct header for inherited secrets', async function (assert) {
+      secret.inherited = true;
+
+      this.setProperties({
+        secret
+      });
+
+      await render(
+        hbs`<Pipeline::SecretsAndTokens::Secrets::Modal::Edit
+            @secret={{this.secret}}
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('.modal-header').hasText('Override inherited secret: TEST ×');
     });
 
     test('it enables submit button correctly', async function (assert) {
@@ -49,6 +106,35 @@ module(
 
       await click('#allow-in-pr-toggle input');
       assert.dom('#submit-secret').isEnabled();
+
+      await click('#allow-in-pr-toggle input');
+      assert.dom('#submit-secret').isDisabled();
+
+      await fillIn('#secret-value-input', 'updated');
+      assert.dom('#submit-secret').isEnabled();
+
+      await click('#allow-in-pr-toggle input');
+      assert.dom('#submit-secret').isEnabled();
+    });
+
+    test('it enables submit button correctly for overriding secret', async function (assert) {
+      secret.inherited = true;
+
+      this.setProperties({
+        secret
+      });
+
+      await render(
+        hbs`<Pipeline::SecretsAndTokens::Secrets::Modal::Edit
+            @secret={{this.secret}}
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('#submit-secret').isDisabled();
+
+      await click('#allow-in-pr-toggle input');
+      assert.dom('#submit-secret').isDisabled();
 
       await click('#allow-in-pr-toggle input');
       assert.dom('#submit-secret').isDisabled();
@@ -124,6 +210,35 @@ module(
       assert.dom('#submit-secret').isDisabled();
       assert.true(closeModalSpy.calledOnce);
       assert.equal(closeModalSpy.calledWith(updatedSecret), true);
+    });
+
+    test('it closes modal with correct argument for overriding secret', async function (assert) {
+      const closeModalSpy = sinon.spy();
+      const overriddenSecret = {
+        name: 'OVERRIDE',
+        allowInPR: false
+      };
+
+      sinon.stub(shuttle, 'fetchFromApi').resolves(overriddenSecret);
+
+      secret.inherited = true;
+
+      this.setProperties({
+        closeModal: closeModalSpy
+      });
+
+      await render(
+        hbs`<Pipeline::SecretsAndTokens::Secrets::Modal::Edit
+            @secret={{this.secret}}
+            @closeModal={{this.closeModal}}
+        />`
+      );
+      await fillIn('#secret-value-input', 'overridden');
+      await click('#submit-secret');
+
+      assert.dom('#submit-secret').isDisabled();
+      assert.true(closeModalSpy.calledOnce);
+      assert.equal(closeModalSpy.calledWith(overriddenSecret), true);
     });
   }
 );
