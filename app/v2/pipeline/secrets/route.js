dcb29930a2b413f34c371f96960c60015e65ab47
Ming Hay Luk <luk.minghay@gmail.com>
2025-05-14 15:28:06 -0700

    Integrate v2 pipeline secrets

--- app/v2/pipeline/secrets/route.js
+++ app/v2/pipeline/secrets/route.js
@@ -2,55 +2,20 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class NewPipelineSecretsRoute extends Route {
-  @service router;
-
   @service session;
 
-  @service shuttle;
+  @service('pipeline-page-state') pipelinePageState;
 
-  @service pipelinePageState;
-
-  async beforeModel(transition) {
+  async beforeModel() {
     // Guests should not access this page
     if (this.session.data.authenticated.isGuest) {
-      this.router.replaceWith('v2.pipeline');
-    } else {
-      this.replaceWith(
-        'pipeline.secrets',
-        transition.to.parent.params.pipeline_id
-      );
+      this.replaceWith('v2.pipeline');
     }
   }
 
-  async model() {
-    // Refresh error message
-    this.controllerFor('v2.pipeline.secrets').set('errorMessage', '');
-
-    const pipeline = this.pipelinePageState.getPipeline();
-    const pipelineId = pipeline.id;
-
-    const secrets = await this.shuttle
-      .fetchFromApi('get', `/pipelines/${pipelineId}/secrets`)
-      .catch(err => {
-        this.controllerFor('v2.pipeline.secrets').set(
-          'errorMessage',
-          err.message
-        );
-
-        return [];
-      });
-
-    const tokens = await this.shuttle
-      .fetchFromApi('get', `/pipelines/${pipelineId}/tokens`)
-      .catch(err => {
-        this.controllerFor('v2.pipeline.secrets').set(
-          'errorMessage',
-          err.message
-        );
-
-        return [];
-      });
-
-    return { pipeline, secrets, tokens };
+  model() {
+    return {
+      pipelineId: this.pipelinePageState.getPipelineId()
+    };
   }
 }
