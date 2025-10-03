7069c10e7f85cc846a0e1f98a8ef76b4f93d483a
Ming Hay Luk <luk.minghay@gmail.com>
2025-10-02 17:05:57 -0700

    Preferences update when pipeline changes

--- app/v2/pipeline/settings/preferences/route.js
+++ app/v2/pipeline/settings/preferences/route.js
@@ -2,11 +2,17 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class V2PipelineSettingsPreferencesRoute extends Route {
+  @service('pipeline-page-state') pipelinePageState;
+
   @service('settings') settings;
 
   async model() {
     if (!this.settings.getSettings()) {
       await this.settings.fetchSettings();
     }
+
+    return {
+      pipelineId: this.pipelinePageState.getPipelineId()
+    };
   }
 }
