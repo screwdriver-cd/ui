75744937368f72de8a966ac6b5cdd124ad0dc0c2
Ming-Hay <157658916+minghay@users.noreply.github.com>
2025-10-03 09:52:09 -0700

    feat: Pipeline settings updates when pipeline changes (#1484)

--- app/v2/pipeline/settings/index/route.js
+++ app/v2/pipeline/settings/index/route.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class V2PipelineSettingsIndexRoute extends Route {}
+export default class V2PipelineSettingsIndexRoute extends Route {
+  @service('pipeline-page-state') pipelinePageState;
+
+  model() {
+    return {
+      pipelineId: this.pipelinePageState.getPipelineId()
+    };
+  }
+}
