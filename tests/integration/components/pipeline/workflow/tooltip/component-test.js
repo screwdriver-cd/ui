966e2ef61e10e89a03ec8b52eb374e595203f81b
yakanechi <138551445+yakanechi@users.noreply.github.com>
2025-11-28 13:26:39 +0900

    fix test

--- tests/integration/components/pipeline/workflow/tooltip/component-test.js
+++ tests/integration/components/pipeline/workflow/tooltip/component-test.js
@@ -273,7 +273,7 @@ module('Integration | Component | pipeline/workflow/tooltip', function (hooks) {
     assert.dom('#hidden-tooltip-description').exists({ count: 1 });
   });
 
-  test.only('it does not render actionable menu options when the node represents stage jobs group', async function (assert) {
+  test('it does not render actionable menu options when the node represents stage jobs group', async function (assert) {
     this.setProperties({
       d3Data: {
         node: {
