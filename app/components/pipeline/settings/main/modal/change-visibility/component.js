7b2a4a8edb790632420d4b5a18a4f932beaba8d3
Akinori Kanechika <nenecchi.dev.oss@gmail.com>
2025-12-17 02:33:41 +0900

    fix(3433): Add change visibility toggle button (#1519)

---
+++ app/components/pipeline/settings/main/modal/change-visibility/component.js
@@ -0,0 +1,41 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+export default class PipelineModalChangeVisibilityComponent extends Component {
+  @service shuttle;
+
+  @service('pipeline-page-state') pipelinePageState;
+
+  @tracked errorMessage = null;
+
+  pipeline;
+
+  constructor() {
+    super(...arguments);
+
+    this.pipeline = this.pipelinePageState.getPipeline();
+  }
+
+  get isPublic() {
+    return this.pipeline.settings?.public ?? false;
+  }
+
+  @action
+  async changeVisibility() {
+    await this.shuttle
+      .fetchFromApi('put', `/pipelines/${this.pipeline.id}`, {
+        settings: {
+          public: !this.isPublic
+        }
+      })
+      .then(pipeline => {
+        this.pipelinePageState.setPipeline(pipeline);
+        this.args.closeModal(true);
+      })
+      .catch(err => {
+        this.errorMessage = err.message;
+      });
+  }
+}
---
+++ tests/integration/components/pipeline/settings/main/modal/change-visibility/component-test.js
@@ -0,0 +1,99 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'screwdriver-ui/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
+
+module(
+  'Integration | Component | pipeline/modal/change-visibility',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    let pipelinePageState;
+
+    hooks.beforeEach(function () {
+      pipelinePageState = this.owner.lookup('service:pipeline-page-state');
+    });
+
+    test('it renders core components', async function (assert) {
+      const pipelineMock = {
+        id: 123
+      };
+
+      sinon.stub(pipelinePageState, 'getPipeline').returns(pipelineMock);
+      this.setProperties({
+        closeModal: () => {}
+      });
+
+      await render(
+        hbs`<Pipeline::Settings::Main::Modal::ChangeVisibility
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('.modal-header').exists({ count: 1 });
+      assert.dom('.alert').doesNotExist();
+      assert.dom('.modal-title').exists({ count: 1 });
+      assert.dom('#change-visibility-message').exists({ count: 1 });
+      assert.dom('.change-visibility-message-warning').exists({ count: 1 });
+      assert.dom('.modal-footer').exists({ count: 1 });
+      assert.dom('#change-visibility-action').exists({ count: 1 });
+    });
+
+    test('it renders when pipeline visibility is false', async function (assert) {
+      const pipelineMock = {
+        id: 123,
+        settings: {
+          public: false
+        }
+      };
+
+      sinon.stub(pipelinePageState, 'getPipeline').returns(pipelineMock);
+      this.setProperties({
+        closeModal: () => {}
+      });
+
+      await render(
+        hbs`<Pipeline::Settings::Main::Modal::ChangeVisibility
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('.modal-header').exists({ count: 1 });
+      assert.dom('.alert').doesNotExist();
+      assert.dom('.modal-title').exists({ count: 1 });
+      assert.dom('#change-visibility-message').exists({ count: 1 });
+      assert.dom('.change-visibility-message-warning').exists({ count: 1 });
+      assert.dom('.modal-footer').exists({ count: 1 });
+      assert.dom('#change-visibility-action').exists({ count: 1 });
+    });
+
+    test('it renders when pipeline visibility is true', async function (assert) {
+      const pipelineMock = {
+        id: 123,
+        settings: {
+          public: true
+        }
+      };
+
+      sinon.stub(pipelinePageState, 'getPipeline').returns(pipelineMock);
+      this.setProperties({
+        closeModal: () => {}
+      });
+
+      await render(
+        hbs`<Pipeline::Settings::Main::Modal::ChangeVisibility
+            @closeModal={{this.closeModal}}
+        />`
+      );
+
+      assert.dom('.modal-header').exists({ count: 1 });
+      assert.dom('.alert').doesNotExist();
+      assert.dom('.modal-title').exists({ count: 1 });
+      assert.dom('#change-visibility-message').exists({ count: 1 });
+      assert.dom('.change-visibility-message-warning').doesNotExist();
+      assert.dom('.modal-footer').exists({ count: 1 });
+      assert.dom('#change-visibility-action').exists({ count: 1 });
+    });
+  }
+);
