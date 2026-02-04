35677dbec8ced18c61ff081c2d5f921c1e1ef291
Akinori Kanechika <nenecchi.dev.oss@gmail.com>
2026-02-04 10:06:56 +0900

    fix(3433): Run Start/Restart Correctly from the Job List (#1553)

--- tests/integration/components/pipeline/jobs/table/cell/actions/component-test.js
+++ tests/integration/components/pipeline/jobs/table/cell/actions/component-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'screwdriver-ui/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 
@@ -14,9 +14,7 @@ module(
         'service:pipeline-page-state'
       );
 
-      sinon
-        .stub(pipelinePageState, 'getPipeline')
-        .returns({ state: 'INACTIVE' });
+      sinon.stub(pipelinePageState, 'getPipeline').returns({ state: 'ACTIVE' });
     });
 
     test('it renders', async function (assert) {
@@ -37,6 +35,38 @@ module(
       );
 
       assert.dom('button').exists({ count: 3 });
+    });
+
+    test('it renders with start and restart modals', async function (assert) {
+      const job = { id: 123, name: 'main', state: 'ENABLED' };
+      const event = {
+        sha: 'abc123def456',
+        commit: { author: { name: 'batman' }, message: 'Some amazing changes' },
+        creator: { name: 'batman' },
+        meta: {}
+      };
+
+      this.setProperties({
+        record: {
+          job,
+          event,
+          canStartFromView: true,
+          onCreate: () => {},
+          onDestroy: () => {}
+        }
+      });
+
+      await render(
+        hbs`<Pipeline::Jobs::Table::Cell::Actions
+            @record={{this.record}}
+        />`
+      );
+      assert.dom('button').exists({ count: 3 });
+      await click('button:nth-of-type(1)');
+      assert.dom('.modal-title').hasText('Are you sure you want to start?');
+
+      await click('button:nth-of-type(3)');
+      assert.dom('.modal-title').hasText('Are you sure you want to restart?');
     });
   }
 );
